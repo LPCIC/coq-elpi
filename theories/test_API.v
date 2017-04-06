b73ed599f24d1fdddd8bb02fda207a7c53e7f082
Enrico Tassi <gares@fettunta.org>
2017-04-06 16:06:24 +0200

    test file for API

---
+++ theories/test_API.v
@@ -0,0 +1,65 @@
+From elpi Require Import elpi.
+
+Elpi Init "./" "../elpi/".
+
+Elpi Accumulate File "pervasives.elpi".
+Elpi Accumulate File "lp-lib.elpi".
+Elpi Accumulate File "coq-lib.elpi".
+
+(****** say *************************************)
+
+Elpi Accumulate "
+test-hello :-
+  coq-say ""hello world"". 
+".
+Elpi Run "test-hello".
+
+(****** locate **********************************)
+
+(* nametab *)
+Elpi Accumulate "
+test-locate-nat :-
+  coq-locate ""nat"" (indt GR),
+  coq-locate ""Datatypes.nat"" (indt GR),
+  coq-locate ""Init.Datatypes.nat"" (indt GR),
+  coq-locate ""Coq.Init.Datatypes.nat"" (indt GR).
+".
+Elpi Run "test-locate-nat".
+
+Elpi Accumulate "
+test-locate-foobar :-
+  coq-locate ""foobar"" _.
+".
+Fail Elpi Run "test-locate-foobar".
+
+(* syndef *)
+Elpi Accumulate "
+test-syndef :-
+  coq-locate ""plus"" (const GR),
+  coq-locate ""Nat.add"" (const GR).
+".
+Elpi Run "test-syndef".
+
+(****** env **********************************)
+
+Elpi Accumulate "
+test-env-const :-
+  coq-locate ""plus"" (const GR),
+  coq-env-const GR BO TY,
+  coq-locate ""nat"" Nat,
+  coq-locate ""S"" Succ,
+  TY = (prod _ Nat _\ prod _ Nat _\ Nat),
+  BO = (fix _ 0 TY add\
+         lam _ Nat n\ lam _ Nat m\
+         match n (lam _ Nat _\ Nat)
+         [ m
+         , lam _ Nat w\ app [Succ, app [add,w,m]]]).
+".
+Elpi Run "test-env-const".
+
+Axiom empty_nat : nat.
+Elpi Run "coq-locate ""empty_nat"" (const GR),
+          coq-env-const GR axiom TY.
+".
+
+
