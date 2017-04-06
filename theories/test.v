aee2fb41d70d8ca821d5f5793119b686e9d8c1ce
Enrico Tassi <gares@fettunta.org>
2017-04-06 17:10:35 +0200

    fix tests

--- theories/test.v
+++ theories/test.v
@@ -6,40 +6,40 @@ Elpi Accumulate File "pervasives.elpi".
 Elpi Accumulate File "lp-lib.elpi".
 Elpi Accumulate File "coq-lib.elpi".
 
-Elpi Accumulate " main2 X :- say nope, X = darling. ".
-Elpi Run "main2 Res, say [my, Res]. ".
+Elpi Accumulate " main2 X :- coq-say nope, X = darling. ".
+Elpi Run "main2 Res, coq-say [my, Res]. ".
 
-Elpi Run "locate ""nat"" X, say X. ".
-Elpi Run "locate ""S"" X, say X. ".
-Elpi Run "locate ""O"" X, say X. ".
-Elpi Run "locate ""S"" S,
-          locate ""O"" O,
+Elpi Run "coq-locate ""nat"" X, coq-say X. ".
+Elpi Run "coq-locate ""S"" X, coq-say X. ".
+Elpi Run "coq-locate ""O"" X, coq-say X. ".
+Elpi Run "coq-locate ""S"" S,
+          coq-locate ""O"" O,
           T = (app [S, app [S, O]]),
-          say T,
+          coq-say T,
           coq-typecheck T TY,
-          say TY,
+          coq-say TY,
           coq-typecheck S STY,
-          say STY,
+          coq-say STY,
           coq-typecheck (lam ""foo"" TY x\app [S, x]) F,
-          say F.
+          coq-say F.
 ".
 
 Elpi Run "
-  locate ""plus"" (const PlusName),
-  say PlusName,
+  coq-locate ""plus"" (const PlusName),
+  coq-say PlusName,
   coq-env-const PlusName Bo Ty,
-  say Ty,
-  say {pp Bo},
+  coq-say Ty,
+  coq-say {pp Bo},
   coq-typecheck Bo Ty.
 ".
 
 
 Elpi Run "
 
-  say {{ nat -> bool }},
-  say {pp x\{{ nat -> lp:x }}},
-  say {pp x\{{ nat -> lp:""x"" }}},
-  {{ lp:"" say say "" }}
+  coq-say {{ nat -> bool }},
+  coq-say {pp x\{{ nat -> lp:x }}},
+  coq-say {pp x\{{ nat -> lp:""x"" }}},
+  {{ lp:"" coq-say say "" }}
 .
 
 ".
@@ -48,19 +48,19 @@ Fail Elpi Run " {{ lp:unknown }} ".
 
 Elpi Run "
 
-  say {{ forall x x, lp:"" {{ x }}  "" -> x }}.
+  coq-say {{ forall x x, lp:"" {{ x }}  "" -> x }}.
 
 ".
 Elpi Run "
 
-  say {{ match (3,4) with (x,y) => x + y end }}.
+  coq-say {{ match (3,4) with (x,y) => x + y end }}.
 
 
 ".
 
 Elpi Run "
 
-  say {{ fix f (n : nat) (m:=0) { struct n } : nat := 
+  coq-say {{ fix f (n : nat) (m:=0) { struct n } : nat := 
            match n with O => m | S k => f k end }}.
 
 ".
@@ -70,12 +70,12 @@ Require Import List.
 Elpi Run "
 
   T = app [ {{ @cons }}, hole, {{ 0 }}, app [ {{ @nil }}, hole ] ],
-  say T,
+  coq-say T,
   coq-elaborate T T1 TTY,
-  say T1,
-  say TTY.
+  coq-say T1,
+  coq-say TTY.
 
 ".
 
-Elpi Run " say {{ cons }}. ".
+Elpi Run " coq-say {{ cons }}. ".
 
