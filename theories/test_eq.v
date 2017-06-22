33e21b06ffaa5819cca65a1ce669e827366b6878
lucas8 <luc.linux@mailoo.org>
2017-06-22 19:08:14 +0200

    add example coq use of equality generation

--- theories/test_eq.v
+++ theories/test_eq.v
@@ -24,10 +24,31 @@ Fixpoint eq_nat (n m : nat) :=
 Definition tmp (f : nat -> nat -> bool)
 (a b : mbtree) := true.
 
-Elpi Accumulate File "eq.elpi".
+Definition tmp' (f : list nat -> list nat -> bool)
+(a b : mbtree) := true.
 
-Elpi Run "test-prod.".
-Elpi Run "test-list.".
+Fixpoint eq_list (A : Type)
+(eq : A -> A -> bool) (l1 l2 : list A) :=
+match (l1,l2) with
+| (cons x1 t1, cons x2 t2) => andb (eq x1 x2)
+                         (eq_list A eq t1 t2)
+| (nil,nil) => true
+| _ => false
+end.
+
+Inductive awful A B :=
+| mkawful : awful (list B) B -> awful A B
+| awful_nil : B -> awful A B.
+
+Inductive mlist A B :=
+| mcons : A -> B -> mlist A B -> mlist A B
+| mnil  : mlist A B.
+
+Elpi Accumulate File "eq.elpi".
+Elpi Run "create-eq-from-name ""mlist"".".
+Check mlist_equal.
+Elpi Run "create-eq-from-name ""prod"".".
+Check prod_equal.
 
 Elpi Run "create-eq-from-name ""nat"".".
 Elpi Accumulate "
@@ -41,8 +62,22 @@ Elpi Accumulate "
 
 Check mbtree_equal.
 
-Inductive awful A :=
-| mkawful : awful (list A) -> awful A
-| awful_nil : A -> awful A.
+Definition eq_pairnat (a b : nat * nat) : bool :=
+match (a,b) with
+| ((x1,y1),(x2,y2)) => andb (eq_nat x1 x2) (eq_nat y1 y2)
+end.
+
+Compute (mbtree_equal eq_pairnat
+  (mbnode (mbleaf (1,2))
+          (mbnode (mbleaf (3,4)) (mbleaf (5,6)) 7)
+          8
+  )
+  (mbnode (mbleaf (1,2))
+          (mbnode (mbleaf (4,4)) (mbleaf (5,6)) 7)
+          8
+  )
+).
+
+
 
 
