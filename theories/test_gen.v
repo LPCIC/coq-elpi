9b85827b591d77ddcc95ad8c0348b46a1b7a337a
lucas8 <luc.linux@mailoo.org>
2017-07-27 11:28:10 +0200

    update generic generation of eq example

--- theories/test_gen.v
+++ theories/test_gen.v
@@ -3,6 +3,19 @@ Elpi Init "./" "./elpi/".
 
 Elpi Accumulate File "pervasives.elpi".
 
+Theorem fg_equal :
+  forall (A B : Type) (f g : A -> B) (x y : A),
+    x = y -> f = g -> f x = g y.
+Proof.
+  intros A B f g x y Hxy Hfg.
+  rewrite <- Hxy. rewrite <- Hfg.
+  reflexivity.
+Qed.
+
+Definition eq_ok (A : Type) (eq : A -> A -> bool) (a b : A) :=
+  (eq a b = true <-> a = b).
+
+
 Inductive LVar A :=
 | VLow : A -> LVar A
 | VHigh : A -> LVar A.
@@ -12,50 +25,27 @@ Inductive LamC (A : Type) :=
 | Abs : LamC (LVar A) -> LamC A
 | Var : A -> LamC A.
 
-(*
-  D = * => *; * => *; e
-  LVar : Ty D (* => *) * = V0 + D0 . V0
-  LamC : Ty D (* => *) * = ((D1 . V0) * (D1 . V0) + D1 . (D0 . V0)) + V0
-  VLow A x  = con 0 (inl x)
-  VHigh A y = con 0 (inr y)
-  App A x y = con 1 (inl (inl (pair x y))
-  Abs A x   = con 1 (inl (inr x))
-  Var A x   = con 1 (inr x)
+Inductive NList (A : Type) :=
+| NCons : NList (A*A) -> NList A
+| NNil.
 
-## Equality generation
+Inductive MList (A B : Type) :=
+| MCons : A -> B -> MList A B -> MList A B
+| MNil : MList A B.
 
-Type :
-  Phi S T = S -> Bool
+Inductive MTree (A : Type) :=
+| MNode : MList A (MTree (A * A)) -> MTree A.
 
-Descriptors :
-  doUnit = \x -> true
-  doPair phi1 phi2 = \(pair x y) -> phi1 x && phi2 y
-  doInl phi = \x -> match x with | Inl y => phi y | Inr y => false end
-  doInr phi = \x -> match x with | Inl y => false | Inr y => phi r end
-  doCon v phi = \(con _ x) -> phi x
-
-## Equality proof
-
-Type :
-  Phi S T = forall (a b : S), eq_ok S eq a b
-
-Descriptors :
-  doUnit = conj (fun H => eq_refl) (fun H => eq_refl)
-  doPair ph1 ph2 = forall ((pair a1 b1) ((pair a2 b2) : S),
-                    conj (proj1 (phi1 a1 b1) ...) (...)
-*)
+Inductive Tp (A B C D : Type) :=
+| C : Tp A (B*B) C (list D) -> Tp A B C D
+| C2 : A -> B -> C -> D -> Tp A B C D.
 
 Elpi Accumulate File "gen.elpi".
 Elpi Accumulate File "geneq.elpi".
-Elpi Run "cleanup-term-test'.".
-
-Inductive SType :=
-| stype : Type -> SType.
-
-Definition from_stype (S : SType) : Type :=
-match S with
-| stype T => T
-end.
-
-Inductive PairType (A B : SType) :=
-| pair : (from_stype A) -> (from_stype B) -> PairType A B.
+Elpi Run "derive-eq ""prod"".".
+Elpi Accumulate "eq-set ""prod"".".
+Elpi Run "derive-eq ""list"".".
+Elpi Accumulate "eq-set ""list"".".
+Elpi Run "derive-eq ""Tp"".".
+Check Tp_equal.
+Print Tp_equal.
