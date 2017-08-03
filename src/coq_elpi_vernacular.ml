94420c499b0181df5b89aa96761ebc9dd97a5d63
Enrico Tassi <gares@fettunta.org>
2017-08-03 10:36:35 +0200

    Elpi Init: use Coq's user-contrib/elpi/ directory by default

--- src/coq_elpi_vernacular.ml
+++ src/coq_elpi_vernacular.ml
@@ -14,6 +14,7 @@ open Names
 (* ******************** Vernacular commands ******************************* *)
 
 let init ~paths =
+  let paths = paths @ [Envars.coqlib()^"/user-contrib/elpi/"] in
   ignore(E.Setup.init List.(flatten (map (fun x -> ["-I";x]) paths)) ".")
 
 let program_src = Summary.ref ~name:"elpi-decls-src" []
