107927680f83e160b178640b578f5f2c3a493850
Enrico Tassi <gares@fettunta.org>
2017-08-09 19:36:30 +0200

    eq: move unit tests to test_eq.v (wip)

--- theories/test_eq.v
+++ theories/test_eq.v
@@ -24,6 +24,93 @@ Inductive mlist (A B : Type) : Type :=
 | mnil  : mlist A B.
 About mlist.
 
+(* unit tests **************************** *)
+
+(* these were written by Luc in the .elpi file
+   but use constants defined in this file, so we
+   put them here (and we run them systematically) *)
+
+Elpi Run derive.eq "
+%build-eq-check-test :-
+   coq-locate ""andb""   And,
+    coq-locate ""true""   True,
+    coq-locate ""nat""    Nat,
+    coq-locate ""O""      Zero,
+    coq-locate ""S""      Succ,
+    TY = {{nat -> nat -> bool}},
+    (pi a\ pi b\ eq-proof Nat (pr a b) _ (app [Eq, a, b]))
+     => build-eq-check And True
+                       [pr Zero Zero, pr Zero Zero]
+                       [Nat, Nat] Out.
+".
+
+Require Import NArith.
+
+Elpi Run derive.eq "
+%build-eq-check-test' :-
+  coq-locate ""andb"" And,
+    coq-locate ""true"" True,
+    coq-locate ""nat""  Nat,
+    coq-locate ""Nat.eqb"" Eq,
+    (pi a\ pi b\ eq-proof Nat (pr a b) _ (app [Eq, a, b]))
+     => build-eq-check And True [] [] Out.
+".
+
+Elpi Run derive.eq "
+%build-eq-check-proof-proj1-eq-test :-
+   coq-locate ""mbtree"" MbTree,
+    coq-locate ""mbnode"" MbNode, MbNode = indc GR,
+    coq-env-indc GR _ _ TY,
+    coq-locate ""eq_refl"" EqRefl', EqRefl = app [EqRefl', hole, hole],
+    coq-locate ""elpi.derive.eq.congr"" FgEq,
+    (pi eq\ pi heq\ pi x1\ pi y1\ pi x2\ pi y2\ pi x3\ pi y3\
+        (pi t\ pi a\ pi b\ eq-proof t (pr a b) (app [heq, t, a, b]) (app [eq, t, a, b])) =>
+        (build-eq-check-proof-proj1-eq MbNode MbNode TY [x1, x2, x3] [y1, y2, y3]
+            [app [eq, MbTree, x1, y1], app [eq, MbTree, x2, y2], app [eq, {{nat}}, x3, y3]]
+            FgEq EqRefl (Out eq heq x1 y1 x2 y2 x3 y3))),
+    Bo = (lam _ {{forall (A : Type), A -> A -> bool}} eq\
+        lam _ {{forall (A : Type) (a b : A), lp:eq A a b = true}} teq\
+        lam _ {{forall (A : Type) (a b : A), eq_ok A (lp:eq A) a b}} heq\
+        lam _ MbTree x1\ lam _ MbTree y1\ lam _ MbTree x2\ lam _ MbTree y2\
+        lam _ {{nat}} x3\ lam _ {{nat}} y3\ Out teq heq x1 y1 x2 y2 x3 y3),
+    coq-elaborate Bo Bo' TBo.
+".
+
+Elpi Run derive.eq "
+%build-eq-check-proof-proj1-eq-test' :-
+   coq-locate ""pair"" Pair, Pair = indc GR,
+    coq-env-indc GR _ _ TY,
+    coq-locate ""eq_refl"" EqRefl', EqRefl = app [EqRefl', hole, hole],
+    coq-locate ""elpi.derive.eq.congr"" FgEq,
+    (pi A\ pi B\ build-eq-match-proof-apply-params-types [A, B] TY (TY' A B)),
+    (pi A\ pi B\ pi ha\ pi hb\ pi x1\ pi y1\ pi x2\ pi y2\
+        (pi t\ pi a\ pi b\ eq-proof t (pr a b) {{DecEq.op_ok lp:a lp:b}} {{DecEq.op lp:a lp:b}}) =>
+        (build-eq-check-proof-proj1-eq (app [Pair, A, B]) (app [Pair, A, B]) (TY' A B )[x1, x2] [y1, y2]
+            [app [ha, x1, y1], app [hb, x2, y2]]
+            FgEq EqRefl (Out A B ha hb x1 y1 x2 y2))),
+    Bo = (lam ""EA"" {{DecEq.type}} EA\ lam ""EB"" {{DecEq.type}} EB\
+        lam ""HA"" {{forall (a b : DecEq.obj lp:EA), DecEq.op a b = true}} ha\
+        lam ""HB"" {{forall (a b : DecEq.obj lp:EB), DecEq.op a b = true}} hb\
+        lam ""x1"" {{DecEq.obj lp:EA}} x1\ lam ""y1"" {{DecEq.obj lp:EA}} y1\
+        lam ""x2"" {{DecEq.obj lp:EB}} x2\ lam ""y2"" {{DecEq.obj lp:EB}} y2\
+        Out {{DecEq.obj lp:EA}} {{DecEq.obj lp:EB}} ha hb x1 y1 x2 y2),
+    coq-elaborate Bo Bo' TBo.
+".
+
+Elpi Run derive.eq "
+%build-eq-check-proof-proj1-test :-
+    coq-locate ""mbnode"" MbNode, MbNode = indc GR,
+    coq-env-indc GR _ _ TY,
+    constructor-args Args' {prod-2-lam TY}, rev Args' Args,
+    (pi h\ pi eq\ pi heq\ pi x1\ pi y1\ pi x2\ pi y2\ pi x3\ pi y3\
+        (pi t\ pi a\ pi b\ eq-proof t (pr a b) (app [heq, t, a, b]) (app [eq, t, a, b])) =>
+        build-eq-check-proof-proj1 MbNode TY Args
+            [x3, x2, x1] [x1, x2, x3] [y3, y2, y1] [y1, y2, y3] [] h (Out h eq heq x1 y1 x2 y2 x3 y3)).
+".
+
+
+(* main tests *************************** *)
+
 Elpi derive.eq prod.
 Fail Elpi derive.eq mbtree.
 Elpi derive.eq nat.
