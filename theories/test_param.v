d75fec2356c6a5924b227be82b8847a960dd2a53
Cyril Cohen <cohen@crans.org>
2017-09-18 17:11:03 +0200

    bridging fix and match

--- theories/test_param.v
+++ theories/test_param.v
@@ -7,8 +7,6 @@ Elpi Tactic param " typecheck. ".
 Elpi Accumulate File "coq-param.elpi".
 Elpi Run param "typecheck".
 
-Fail Elpi Run param "derive-param ""plus""".
-
 Inductive natR : nat -> nat -> Type :=
 | O_R : natR 0 0
 | S_R : forall (m n : nat), natR m n -> natR (S m) (S n).
@@ -24,55 +22,99 @@ eq_refl_R : @eqR _ _ _ _ _ xR _ _ xR.
 Elpi Accumulate "param {{eq}} {{eq}} {{@eqR}}.".
 Elpi Accumulate "param {{eq_refl}} {{eq_refl}} {{@eq_refl_R}}.".
 
+Class param X XR (x : X) (xR : XR).
+(* Hint Extern 0 (param _ _ _ _) => *)
+(*   match goal with param _ _ ?x _ => elpi "derive-param" *)
 
-Elpi Run param "derive-param ""pred""".
+Definition nat2nat := nat -> nat.
+Elpi Run param "env-add-param ""nat2nat"" ""nat2natR""".
+Elpi Run param "env-add-param ""pred"" ""predR""".
+Print predR.
+
+Check (predR : nat2natR pred pred).
 
 Fixpoint predn n := match n with 0 => 0 | S n => S (predn n) end.
 
-Fail Elpi Run param "derive-param ""predn""".
+Fail Elpi Run param "derive-param ""predn"" _ _".
 
-Fixpoint prednR (n n0 : nat) (n1 : natR n n0) :=
- match
+Fixpoint quasidn n m := S (match n with 0 => m | S n => S (quasidn n m) end).
+
+Fail Elpi Run param "derive-param ""quasidn"" _ _".
+
+Require Import ssreflect ssrfun ssrbool.
+
+Definition M prednR (n n0 : nat) (n1 : natR n n0) :=  match
    n1 in (natR n2 n3)
    return
      (natR
         match n2 with
         | 0 => 0
-        | S n5 =>
-            S
-              ((fix predn (n6 : nat) : nat :=
-                  match n6 with
-                  | 0 => 0
-                  | S n7 => S (predn n7)
-                  end) n5)
+        | S n5 => S (predn n5)
         end
         match n3 with
         | 0 => 0
-        | S n5 =>
-            S
-              ((fix predn (n6 : nat) : nat :=
-                  match n6 with
-                  | 0 => 0
-                  | S n7 => S (predn n7)
-                  end) n5)
+        | S n5 => S (predn n5)
         end)
  with
  | O_R => O_R
  | S_R n2 n3 n4 =>
-     S_R
-       ((fix predn (n5 : nat) : nat :=
-           match n5 with
-           | 0 => 0
-           | S n6 => S (predn n6)
-           end) n2)
-       ((fix predn (n5 : nat) : nat :=
-           match n5 with
-           | 0 => 0
-           | S n6 => S (predn n6)
-           end) n3) (prednR n2 n3 n4)
+     S_R (predn n2) (predn n3) (prednR n2 n3 n4)
  end.
 
-Fail Elpi Run param "derive-param ""plus""".
-Fail Elpi Run param "derive-param ""eq_rect""".
+Fixpoint prednR' (n n0 : nat) (n1 : natR n n0) : natR (predn n) (predn n0).
+Proof.
+pose M := @M prednR' n n0 n1.
+move: n0 n1 @M.
+case: n=> [|n']; case=> [|n0'] n1 M; exact: M.
+Defined.
+
+Eval compute - [predn M] in prednR'.
+
+
+Fixpoint prednR (n n0 : nat) (n1 : natR n n0) :=
+ let M : (forall n n0 (n1 : natR n n0), natR
+        match n with
+        | 0 => 0
+        | S n5 => S (predn n5)
+        end
+        match n0 with
+        | 0 => 0
+        | S n5 => S (predn n5)
+        end) := fun n n0 n1 => match
+   n1 in (natR n2 n3)
+   return
+     (natR
+        match n2 with
+        | 0 => 0
+        | S n5 => S (predn n5)
+        end
+        match n3 with
+        | 0 => 0
+        | S n5 => S (predn n5)
+        end)
+ with
+ | O_R => O_R
+ | S_R n2 n3 n4 =>
+     S_R (predn n2) (predn n3) (prednR n2 n3 n4)
+ end in
+match n as n2 return (forall n3 : nat, natR n2 n3 -> natR (predn n2) (predn n3)) with
+         | 0 =>
+             fun abs_n2 : nat =>
+             match abs_n2 as n2 return (natR 0 n2 -> natR (predn 0) (predn n2)) with
+             | 0 => M 0 0
+             | S n0' => M 0 (S n0')
+             end
+         | S n' =>
+             fun abs_n2 : nat =>
+             match abs_n2 as n2 return (natR (S n') n2 -> natR (predn (S n')) (predn n2)) with
+             | 0 =>  M (S n') 0
+             | S n0' => M (S n') (S n0')
+             end
+         end n0 n1.
+
+
+
+Fail Elpi Run param "derive-param ""plus"" _ _".
+Fail Elpi Run param "derive-param ""eq_rect"" _ _".
 
 Goal True.
