e528c599fba44d458d3c6e1fb4cd2fd0c4a79d2e
Enrico Tassi <gares@fettunta.org>
2017-11-02 19:41:53 +0100

    elpi.v: eq_f made more polymorphic

--- theories/elpi.v
+++ theories/elpi.v
@@ -11,7 +11,7 @@ exact (fun h T =>
   eq_rect true (fun x => match x with false => T | _ => True end) I false h).
 Qed.
 
-Lemma eq_f (T : Type) (f : T -> bool) a b : a = b -> f a = f b.
+Lemma eq_f (T1 : Type) (T2 : Type) (f : T1 -> T2) a b : a = b -> f a = f b.
 Proof.
 exact (fun h =>
   eq_rect a (fun x => f a = f x) (eq_refl (f a)) b h).
