9efd17e06345312b3a45ad8ba49bf3c853562d02
Enrico Tassi <gares@fettunta.org>
2017-12-08 18:37:16 +0100

    derive/map: map a function over a container

---
+++ theories/derive/map.v
@@ -0,0 +1,22 @@
+Require Import elpi.
+
+Elpi Db derive.map.db "type map-db term -> term -> prop.".
+
+(** A map over a container. If the container has a parameter that is
+    used to type an index, then such parameter is not mapped. E.g.
+      Inductive I A B : A -> Type := K : forall a b, I A B a.
+    Elpi derive.map I.
+      Definition I_map A B C (f : B -> C) a : I A B a -> I A B a.
+*)
+Elpi Command derive.map.
+Elpi Accumulate Db derive.map.db.
+Elpi Accumulate File "derive/map.elpi".
+Elpi Accumulate "
+  main [str I] :- !,
+    coq-locate I T,
+    if (T = indt GR) (derive-map GR) usage.
+  main _ :- usage.
+
+  usage :- coq-error ""Usage: derive.map <inductive type name>"".
+".  
+Elpi Typecheck.
---
+++ theories/derive/tests/test_map.v
@@ -0,0 +1,19 @@
+From elpi Require Import derive.map derive.param1 derive.projK.
+
+Require Vector.
+
+Elpi derive.map list.
+Elpi derive.map prod.
+Elpi derive.map ex.
+Elpi derive.map Vector.t.
+
+Inductive blurb A := K1 (a:A) | K2 (b: list A) (c:blurb A).
+
+Elpi derive.map blurb.
+
+Elpi derive.param1 prod prodR.
+Elpi derive.param1 list listR.
+
+Elpi derive.map prodR.
+
+Elpi derive.map listR.
