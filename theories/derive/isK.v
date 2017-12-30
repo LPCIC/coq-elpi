472922229d2042ccdbad754655068c686b49b935
Enrico Tassi <gares@fettunta.org>
2017-12-30 22:46:52 +0100

    derive.isK: use a Db

--- theories/derive/isK.v
+++ theories/derive/isK.v
@@ -3,7 +3,10 @@ From elpi Require Import elpi.
 (** derive.isK generates a function per constructor that returns
     true iff it is applied to such constructor *)
 
+Elpi Db derive.isK.db " type isK-db @gref -> term -> prop. ".
+
 Elpi Command derive.isK.
+Elpi Accumulate Db derive.isK.db.
 Elpi Accumulate File "derive/isK.elpi".
 Elpi Accumulate "
   main [str I] :- !,
--- theories/ltac/discriminate.v
+++ theories/ltac/discriminate.v
@@ -3,6 +3,7 @@ From elpi Require Export derive.isK.
 (** A tactic proving the current goal out of a false equation *)
 
 Elpi Tactic discriminate.
+Elpi Accumulate Db derive.isK.db.
 Elpi Accumulate File "ltac/discriminate.elpi".
 Elpi Accumulate "
   solve [trm E] [goal Ctx Ev Ty _] [] :- !,
