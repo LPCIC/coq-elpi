7717815fce7fd5133bdb57c9423e022ea1b20543
Enrico Tassi <gares@fettunta.org>
2017-12-30 22:47:54 +0100

    derive.param1: use a Db

--- theories/derive/param1.v
+++ theories/derive/param1.v
@@ -4,8 +4,11 @@ Require Import elpi.
 Class reali_db {X XR : Type} (x : X) (xR : XR) := store_reali {}.
 Class reali {X : Type} {XR : X -> Type} (x : X) (xR : XR x) := Reali {}.
 
+Elpi Db derive.param1.db " type param1-db term -> term -> prop. ".
+
 Elpi Command derive.param1.
-Elpi Accumulate File "coq-lib-extra.elpi" .
+Elpi Accumulate File "coq-lib-extra.elpi".
+Elpi Accumulate Db derive.param1.db.
 Elpi Accumulate File "derive/param1.elpi".
 Elpi Accumulate "
   main [str I, str O] :- !, derive-param1 I O.
