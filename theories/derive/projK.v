2e26146f05ab0ba9e9fff5fff5ab887e8ed59731
Enrico Tassi <gares@fettunta.org>
2017-12-31 20:46:51 +0100

    derive.projK: use a db

--- theories/derive/projK.v
+++ theories/derive/projK.v
@@ -3,7 +3,10 @@ From elpi Require Import elpi.
 (** derive.projK generates a projection for each argument of each constructor.
     the projection is expected to be applied to an explicit constructor. *)
 
+Elpi Db derive.projK.db " type projK-db @gref -> int -> term -> prop. ".
+
 Elpi Command derive.projK.
+Elpi Accumulate Db derive.projK.db.
 Elpi Accumulate File "derive/projK.elpi".
 Elpi Accumulate "
   main [str I] :- !,
--- theories/ltac/injection.v
+++ theories/ltac/injection.v
@@ -3,12 +3,13 @@ From elpi Require Export derive.projK.
 (** A tactic pushing an equation under a constructor *)
 
 Elpi Tactic injection.
+Elpi Accumulate Db derive.projK.db.
 Elpi Accumulate File "ltac/injection.elpi".
 Elpi Accumulate "
   solve [trm E] [goal Ctx _ _ _ as G] NG :- !,
     Ctx => (of E Eq ER, !, injection ER Eq _ P),
-    refine (app[hole|P]) G NG,
-    print_constraints.
+    if (P = []) (coq-error ""Could not generate new equations"")
+       (refine (app[hole|P]) G NG).
 
   solve _ _ _ :- usage.
 
