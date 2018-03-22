4e9b392307e94f146d242401315b2be5a98d3387
Enrico Tassi <gares@fettunta.org>
2018-03-22 18:53:18 +0100

    fixup eq

--- theories/derive/eq.v
+++ theories/derive/eq.v
@@ -7,10 +7,10 @@ Elpi Command derive.eq.
 Elpi Accumulate Db derive.eq.db.
 Elpi Accumulate File "derive/eq.elpi".
 Elpi Accumulate "
-  main [str I, str O] :- !, derive.eq.main I O.
+  main [str I, str O] :- !, derive.eq.main I O _.
   main [str I] :- !, 
     coq.locate I T, term->gr T GR, coq.gr->id GR Id, O is Id ^ ""_eq"",
-    derive.eq.main I O.
+    derive.eq.main I O _.
   main _ :- usage.
 
   usage :- coq.error ""Usage: derive.eq <inductive type name> [<output name>]"".
