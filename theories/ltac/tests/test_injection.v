ab556da7934626fccb2738671ce85b91b5e1e2b6
Enrico Tassi <gares@fettunta.org>
2018-04-16 15:33:00 +0200

    fixup injection

--- theories/ltac/tests/test_injection.v
+++ theories/ltac/tests/test_injection.v
@@ -6,10 +6,28 @@ Set Implicit Arguments.
 
 Elpi derive.projK nat.
 
-Lemma test (a b :nat) : S a = S b -> a = b.
+Lemma test_nat (a b :nat) : S a = S b -> a = b.
 Proof.
 intro H.
 elpi injection (H).
 intro I.
 exact I.
+Qed.
+
+Require Vector.
+Require Import ssreflect Arith.
+
+Elpi derive.projK Vector.t.
+
+
+Lemma test_vect A a b n (v1 v2 : Vector.t A n) :
+  Vector.cons A a n v1 = Vector.cons A b n v2 -> a = b /\ v1 = v2.
+Proof.
+intro H.
+elpi injection (H).
+move=> /= Eab _ Esigv12.
+split.
+  exact Eab.
+rewrite -[v2](projT2_eq Esigv12) /=.
+by rewrite (UIP_nat _ _ (projT1_eq Esigv12) (eq_refl n)).
 Qed.
