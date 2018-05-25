aab704ac753d688587dd3b7f7234b57ddd0a7c40
Enrico Tassi <gares@fettunta.org>
2018-05-25 16:41:41 +0200

    induction: major change, not based on truncated elim for param1 T
    The induction for T is the truncated induction for (param1 T)
    where TR is replaced by P (the 2nd order variable) in all hypotheses.

--- theories/derive/tests/test_induction.v
+++ theories/derive/tests/test_induction.v
@@ -1,49 +1,58 @@
 From elpi Require Import derive.induction derive.param1 derive.param1P.
 From Coq Require Vector.
 
+Elpi derive.param1 nat.
 Elpi derive.induction nat.
 
 Check nat_induction : 
   forall P : nat -> Type,
    P O ->
    (forall n, P n -> P (S n)) ->
-   forall x, P x.
+   forall x, natR x -> P x.
 
+Elpi derive.param1 list.
 Elpi derive.induction list.
 
 Check list_induction :
-  forall A : Type, forall P : list A -> Type,
+  forall (A : Type) (PA : A -> Type) (P : list A -> Type),
     P (@nil A) ->
-    (forall x : A, UnitPred A x -> forall xs : list A, P xs -> P (@cons A x xs)) ->
-    forall l, P l.
+    (forall x : A, PA x -> forall xs : list A, P xs -> P (@cons A x xs)) ->
+    forall l, listR A PA l -> P l.
 
-Elpi derive.param1 nat.
 Elpi derive.param1P natR.
-Elpi derive.induction Vector.t.
 
-Check t_induction :
-  forall A : Type, forall P : forall n : nat, Vector.t A n -> Type,
-    P 0 (Vector.nil A) ->
-    (forall a : A, UnitPred A a -> forall n : nat, natR n ->
-     forall v : Vector.t A n, P n v -> P (S n) (Vector.cons A a n v)) ->
-  forall l (x : Vector.t A l), P l x.
+Module V.
+Include Vector.
+Elpi derive.param1 t.
+Elpi derive.induction Vector.t "induction".
+End V.
 
+Check V.induction :
+  forall A (PA : A -> Type) (P : forall n : nat, natR n -> V.t A n -> Type),
+    P 0 OR (Vector.nil A) ->
+    (forall a : A, PA a -> forall n : nat, forall nR: natR n,
+     forall v : V.t A n, P n nR v -> P (S n) (SR n nR) (V.cons A a n v)) ->
+  forall l lR (x : V.t A l), V.tR A PA l lR x -> P l lR x.
+
+Module N.
 Inductive nat1 := 
  | O (_ : bool)
  | S (_ : nat1 * (bool * list nat1)) (b : bool * bool).
 
 Elpi derive.param1 bool.
-Elpi derive.param1 list.
 Elpi derive.param1 prod.
 Elpi derive.param1P boolR.
 Elpi derive.param1P listR.
 Elpi derive.param1P prodR.
+
+Elpi derive.param1 nat1.
 Elpi derive.induction nat1.
 
-Check nat1_induction : forall P : nat1 -> Type,
+Check nat1_induction : 
+     forall (P : nat1 -> Type),
        (forall b : bool, boolR b -> P (O b)) ->
-       (forall a : nat1 * (bool * list nat1),
-        prodR nat1 P (bool * list nat1) 
-         (prodR bool boolR (list nat1) (listR nat1 P)) a ->
-        forall b : bool * bool, prodR bool boolR bool boolR b -> P (S a b)) ->
-       forall s : nat1, P s.
+       (forall x : nat1 * (bool * list nat1),
+        prodR nat1 nat1R (bool * list nat1) (prodR bool boolR (list nat1) (listR nat1 nat1R)) x ->
+        forall b : bool * bool, prodR bool boolR bool boolR b -> P (S x b)) ->
+       forall n : nat1, nat1R n -> P n.
+End N.
