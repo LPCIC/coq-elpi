ca13d8d7e2e4bd2fedeae8b6cae6c844ac737122
Enrico Tassi <gares@fettunta.org>
2018-06-04 18:58:28 +0200

    derive: enable map on param1

--- theories/derive/tests/test_derive.v
+++ theories/derive/tests/test_derive.v
@@ -1,11 +1,11 @@
-From elpi Require Import derive derive.param1 derive.induction.
+From elpi Require Import derive.
 
 Elpi derive nat.
 
 Check nat.eq : nat -> nat -> bool.
 Check nat.is.O : nat -> bool.
 Check nat.is.S : nat -> bool.
-Fail Check nat.map : nat -> nat.
+Check nat.map : nat -> nat.
 Check nat.injection.S1 : nat -> nat -> nat.
 Check nat.param1.nat : nat -> Type.
 Check nat.param1.O : nat.param1.nat O.
@@ -14,6 +14,7 @@ Check nat.param1.natP : forall x, nat.param1.nat x.
 Check nat.param1.inv.nat : nat -> Type.
 Check nat.param1.inv.O : forall i, 0 = i -> nat.param1.inv.nat i.
 Check nat.param1.inv.S : forall i, forall y x, y = x -> nat.param1.inv.nat x -> S y = i -> nat.param1.inv.nat i.
+Check nat.param1.map : forall x, nat.param1.nat x -> nat.param1.nat x.
 Check nat.induction.principle : forall P, P 0 -> (forall n, P n -> P (S n)) -> forall x, nat.param1.nat x -> P x.
 Check nat.induction : forall P, P 0 -> (forall n, P n -> P (S n)) -> forall x, P x.
 
@@ -27,6 +28,7 @@ Check list.injection.cons1 : forall A, A -> list A -> list A -> A.
 Check list.injection.cons2 : forall A, A -> list A -> list A -> list A.
 Check list.param1.nil : forall A P, list.param1.list A P (@nil A).
 Check list.param1.cons : forall A P x (Px : P x) tl (Ptl : list.param1.list A P tl), list.param1.list A P (cons x tl).
+Check list.param1.map : forall A P Q, (forall x, P x -> Q x) -> forall l, list.param1.list A P l -> list.param1.list A Q l.
 Check list.induction.principle : forall A PA P, P nil -> (forall x, PA x -> forall xs, P xs -> P (cons x xs)) -> forall l, list.param1.list A PA l -> P l.
 Check list.induction : forall A P, P nil -> (forall x xs, P xs -> P (cons x xs)) -> forall l, P l.
 
@@ -35,7 +37,7 @@ Require Vector.
 Elpi derive Vector.t Vector_t.
 Check Vector_t.eq : forall A, (A -> A -> bool) -> forall n, Vector.t A n -> Vector.t A n -> bool.
 Check Vector_t.is.nil : forall A n, Vector.t A n -> bool.
-Check Vector_t.is.cons : forall A n, Vector.t A n -> bool.
+Check Vector_t.is.cons : forall A n, Vector.t A n -> bool. 
 Check Vector_t.map : forall A B, (A -> B) -> forall n, Vector.t A n -> Vector.t B n.
 Check Vector_t.injection.cons1 : forall A n, A -> forall m, Vector.t A m -> Vector.t A n -> A.
 Check Vector_t.injection.cons2 : forall A n, A -> forall m, Vector.t A m -> Vector.t A n -> nat.
