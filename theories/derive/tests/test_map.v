d6661b389d6d420fe4518a212c24ba6c12f5274a
Enrico Tassi <Enrico.Tassi@Inria.fr>
2019-02-04 15:56:02 +0100

    cleanup test_map

--- theories/derive/tests/test_map.v
+++ theories/derive/tests/test_map.v
@@ -1,45 +1,90 @@
 From elpi Require Import derive.map derive.param1 derive.projK.
 
-From elpi.derive Require Import
-  test_derive_stdlib test_param1.
+From elpi.derive Require Import test_derive_stdlib test_param1.
+Import test_derive_stdlib.Coverage.
+Import test_param1.Coverage.
 
 Module Coverage.
 
-Elpi derive.map Coverage.empty.
-Elpi derive.map Coverage.unit.
-Elpi derive.map Coverage.peano.
-Elpi derive.map Coverage.option.
-Elpi derive.map Coverage.pair.
-Elpi derive.map Coverage.seq.
-Elpi derive.map Coverage.rose.
-Fail Elpi derive.map Coverage.nest.
-Fail Elpi derive.map Coverage.w.
-Elpi derive.map Coverage.vect.
-Elpi derive.map Coverage.dyn.
-Fail Elpi derive.map Coverage.zeta.
-Fail Elpi derive.map Coverage.beta.
-Elpi derive.map Coverage.iota.
-Elpi derive.map Coverage.large.
-
-
-Elpi derive.map Coverage.is_empty.
-Elpi derive.map Coverage.is_unit.
-Elpi derive.map Coverage.is_peano.
-Elpi derive.map Coverage.is_option.
-Elpi derive.map Coverage.is_pair.
-Elpi derive.map Coverage.is_seq.
-Elpi derive.map Coverage.is_rose.
-Elpi derive.map Coverage.is_nest.
-Fail Elpi derive.map Coverage.is_w.
-
-Elpi derive.map Coverage.is_vect.
-Elpi derive.map Coverage.is_dyn.
-Fail Elpi derive.map Coverage.is_zeta.
-Fail Elpi derive.map Coverage.is_beta.
-Elpi derive.map Coverage.is_iota.
-(* Elpi derive.map Coverage.largeR. exponential because of search *) 
+Elpi derive.map empty.
+Elpi derive.map unit.
+Elpi derive.map peano.
+Elpi derive.map option.
+Elpi derive.map pair.
+Elpi derive.map seq.
+Elpi derive.map rose.
+Fail Elpi derive.map nest.
+Fail Elpi derive.map w.
+Elpi derive.map vect.
+Elpi derive.map dyn.
+Fail Elpi derive.map zeta.
+Fail Elpi derive.map beta.
+Elpi derive.map iota.
+Elpi derive.map large.
 
 End Coverage.
 
-Check Coverage.is_seq_map :
-  forall A P Q, (forall x, P x -> Q x) -> forall l, Coverage.is_seq A P l -> Coverage.is_seq A Q l.
+Import Coverage.
+
+Notation map T := (T -> T).
+Notation map1 T := (forall X Y, (X -> Y) -> T X%type -> T Y%type).
+
+Check empty_map : map empty.
+Check unit_map : map unit.
+Check peano_map : map peano.
+Check option_map : map1 option.
+Check pair_map : forall A B (f : A -> B) C D (g : C -> D), (pair A C) -> (pair B D).
+Check seq_map : map1 seq.
+Check rose_map : map1 rose.
+Fail Check nest_map.
+Fail Check w_map.
+Check vect_map : forall A B (f : A -> B) i, vect A i -> vect B i.
+Check dyn_map : map dyn.
+Fail Check zeta_map.
+Fail Check beta_map.
+Check iota_map : map iota.
+Check large_map : map large.
+
+Module Coverage1.
+
+Elpi derive.map is_empty.
+Elpi derive.map is_unit.
+Elpi derive.map is_peano.
+Elpi derive.map is_option.
+Elpi derive.map is_pair.
+Elpi derive.map is_seq.
+Elpi derive.map is_rose.
+Elpi derive.map is_nest.
+Fail Elpi derive.map is_w.
+
+Elpi derive.map is_vect.
+Elpi derive.map is_dyn.
+Fail Elpi derive.map is_zeta.
+Fail Elpi derive.map is_beta.
+Elpi derive.map is_iota.
+Fail Timeout 1 Elpi derive.map is_large. (* exponential *)
+
+End Coverage1.
+
+Notation func isT := (forall x, isT x -> isT x).
+Notation func1 isT := (forall A P Q, (forall y : A, P y -> Q y) -> forall x, isT A P x -> isT A Q x).
+Notation func2 isT := (forall A P Q, (forall y : A, P y -> Q y) -> forall A1 P1 Q1, (forall y : A1, P1 y -> Q1 y) -> forall x, isT A P A1 P1 x -> isT A Q A1 Q1 x).
+
+Import Coverage1.
+
+Check is_empty_map : func is_empty.
+Check is_unit_map : func is_unit.
+Check is_peano_map : func is_peano.
+Check is_option_map : func1 is_option.
+Check is_pair_map : func2 is_pair.
+Check is_seq_map : func1 is_seq.
+Check is_rose_map : func1 is_rose.
+Fail Check is_nest_map : func1 is_nest.
+Fail Check is_w_map.
+
+Check is_vect_map : forall A P Q, (forall y : A, P y -> Q y) -> forall i p (v : vect A i), is_vect A P i p v -> is_vect A Q i p v.
+Check is_dyn_map : func is_dyn.
+Fail Check is_zeta_map.
+Fail Check is_beta_map.
+Check is_iota_map : func is_iota.
+Fail Check is_large_map.
