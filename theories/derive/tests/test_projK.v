278a10e20a9de9c9b821de6689f9ccae42d7d3ff
Enrico Tassi <Enrico.Tassi@Inria.fr>
2019-02-04 18:00:51 +0100

    clean up test projK

--- theories/derive/tests/test_projK.v
+++ theories/derive/tests/test_projK.v
@@ -1,58 +1,49 @@
 From elpi Require Import test_derive_stdlib derive.projK.
 
+Import test_derive_stdlib.Coverage.
+
 Module Coverage.
-Elpi derive.projK Coverage.empty.
-Elpi derive.projK Coverage.unit.
-Elpi derive.projK Coverage.peano.
-Elpi derive.projK Coverage.option.
-Elpi derive.projK Coverage.pair.
-Elpi derive.projK Coverage.seq.
-Elpi derive.projK Coverage.rose.
-Elpi derive.projK Coverage.nest.
-Elpi derive.projK Coverage.w.
-Elpi derive.projK Coverage.vect.
-Elpi derive.projK Coverage.dyn.
-Elpi derive.projK Coverage.zeta.
-Elpi derive.projK Coverage.beta.
-Elpi derive.projK Coverage.iota.
-Elpi derive.projK Coverage.large.
+Elpi derive.projK empty.
+Elpi derive.projK unit.
+Elpi derive.projK peano.
+Elpi derive.projK option.
+Elpi derive.projK pair.
+Elpi derive.projK seq.
+Elpi derive.projK rose.
+Elpi derive.projK nest.
+Elpi derive.projK w.
+Elpi derive.projK vect.
+Elpi derive.projK dyn.
+Elpi derive.projK zeta.
+Elpi derive.projK beta.
+Elpi derive.projK iota.
+Elpi derive.projK large.
 End Coverage.
-Set Implicit Arguments.
 
-Elpi derive.projK nat.
+Import Coverage.
 
-Lemma test_proj1S x : projS1 33 (S x) = x.
-Proof. split. Qed.
-
-Require Vector.
-
-Elpi derive.projK Vector.t. Check projcons3.
-
-Arguments Vector.nil {_}.
-Inductive Box A : nat -> Type :=
- | mkB (T : Type) (a : A) i (t : Vector.t T i) : Box A (S i)
- | Oops : Box A 0.
-
-Elpi derive.projK Box.
-
-Lemma test_projmkB1 A (a : A) d1 d2 d3 d4 :
-  @projmkB1 A 1 d1 d2 d3 d4 (@mkB A bool a 0 Vector.nil) = bool.
-Proof. split. Qed.
-
-Lemma test_projmkB2 A (a : A) d1 d2 d3 d4 :
-  @projmkB2 A _ d1 d2 d3 d4 (@mkB A bool a 0 Vector.nil) = a.
-Proof. split. Qed.
-
-Lemma test_projmkB3 A (a : A) d1 d2 d3 d4 :
-  @projmkB3 A _ d1 d2 d3 d4 (@mkB A bool a 0 Vector.nil) = 0.
-Proof. split. Qed.
-
-Lemma test_projmkB4 A (a : A) d1 d2 d3 d4 :
-  @projmkB4 A _ d1 d2 d3 d4 (@mkB A bool a 0 Vector.nil) =
-      existT (fun T => { i : nat & Vector.t T i}) bool
-        (existT (fun i => Vector.t bool i) 0 Vector.nil).
-Proof. split. Qed.
-
-Inductive rtree A : Type :=
-  Leaf (n : A) | Node (l : list (rtree A)).
-Elpi derive.projK rtree.
+Check projSucc1 : peano -> peano -> peano.
+Check projSome1 : forall A, A -> option A -> A.
+Check projComma1 : forall A B, A -> B -> pair A B -> A.
+Check projComma2 : forall A B, A -> B -> pair A B -> B.
+Check projCons1 : forall A, A -> seq A -> seq A -> A.
+Check projCons2 : forall A, A -> seq A -> seq A -> seq A.
+Check projNode1 : forall A, seq (rose A) -> rose A -> seq (rose A).
+Check projConsN1 : forall A, A -> nest (pair A A) -> nest A -> A.
+Check projConsN2 : forall A, A -> nest (pair A A) -> nest A -> nest (pair A A).
+Check projvia1 : forall A, (A -> w A) -> w A -> (A -> w A).
+Check projVCons1 : forall A i, A -> forall j, vect A j -> vect A i -> A.
+Check projVCons2 : forall A i, A -> forall j, vect A j -> vect A i -> peano.
+Check projVCons3 : forall A i, A -> forall j, vect A j -> vect A i -> { w & vect A w }.
+Check projbox1 : forall T, T -> dyn -> Type.
+Check projbox2 : forall T, T -> dyn -> { T : Type & T }.
+Fail Check projEnvelope1 : forall A, A -> zeta A -> A.
+Check projRedex1 : forall A, A -> beta A -> A.
+Check projWhy1 : forall n : peano, match n return Type with 
+                    | Zero => peano
+                    | Succ _ => unit
+                    end -> iota -> peano.
+Check projWhy2 : forall n : peano, match n return Type with 
+                    | Zero => peano
+                    | Succ _ => unit
+                    end -> iota -> { i : peano & match i with Zero => peano | Succ _ => unit end }.
