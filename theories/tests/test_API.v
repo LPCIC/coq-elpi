63169ed8192eff2a5265b8b1649b1af7c31c45f5
Enrico Tassi <Enrico.Tassi@Inria.fr>
2019-10-04 17:55:30 +0200

    fix version parsing

--- theories/tests/test_API.v
+++ theories/tests/test_API.v
@@ -37,7 +37,10 @@ Elpi Query lp:{{
 
 Elpi Query lp:{{
   coq.version V MA MI P,
-  coq.say V MA MI P.
+  std.assert! (MA = 8 ; MA = 9) "Coq major version not 8 or 9",
+  std.assert! (MI >= 0 ; MI < 20) "Coq minor version not in 0 - 20",
+  std.assert! (P >= 0 ; P > -5) "Coq never made so many betas",
+  coq.say "Coq version:" V "=" MA "." MI "." P.
 }}.
 
 
