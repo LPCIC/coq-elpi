9441602d7b8bfe416f8d7517baa01afb7a844121
Enrico Tassi <Enrico.Tassi@Inria.fr>
2020-08-25 18:53:32 +0200

    more doc

--- apps/derive/theories/derive.v
+++ apps/derive/theories/derive.v
@@ -1,7 +1,46 @@
 (* Generates a module containing all the derived constants.
 
-   license: GNU Lesser General Public License Version 2.1 or later           
+   license: GNU Lesser General Public License Version 2.1 or later
    ------------------------------------------------------------------------- *)
+
+(** The derive command
+   The derive command can be invoked in two ways.
+   - [derive <inductive-type> <prefix>]
+   - [derive Inductive <declaration>]
+
+   The first command runs all the derivations on an alerady declared inductive
+   type named [<inductive-type>] and all generated constants are named after the
+   prefix [<prefix>] (by default the inductive type name followed by [_]). Example:
+
+<<
+      derive nat. (* default prefix nat_ *)
+      derive nat my_nat_stuff_.
+>>
+
+   The second command takes as argument an inductive type declaration, it
+   creates a module named after the inductive type and puts inside id both
+   the inductive types and the output of the derivations. Example:
+
+<<
+     derive Inductive tickle A := stop | more : A -> tickle-> tickle.
+>>
+
+   is equivalent to
+
+<<
+     Module tickle.
+     Inductive tickle A := stop | more : A -> tickle-> tickle.
+     derive tickle "".
+     End tickle.
+     Notation tickle := tickle.tickle.
+     Notation stop := tickle.stop.
+     Notation more := tickle.more.
+>>
+
+  Both commands honor the [#[verbose]] attribute. If set they print all
+  the derivations that are run, and if they fail or succeed.
+
+*)
 
 From elpi.apps Require Export
   derive.eq
@@ -85,7 +124,7 @@ Elpi Accumulate lp:{{
 pred with-attributes i:prop.
 with-attributes P :-
   attributes A,
-  coq.parse-attributes A [att "verbose" bool, att "flat" bool] Opts, !,
+  coq.parse-attributes A [att "verbose" bool] Opts, !,
   Opts => P.
 
 main [str I, str Prefix] :- !,
