d3b4b82aee3b3f2d2f0d465cf45e66c92e7b275f
Enrico Tassi <Enrico.Tassi@Inria.fr>
2020-08-28 14:18:41 +0200

    it works!

--- apps/NES/examples/usage_NES.v
+++ apps/NES/examples/usage_NES.v
@@ -30,3 +30,16 @@ Eval compute in This.Is.A.Long.Namespace.more_stuff. (* = 1 *)
 (* For convenience one can open a namespace to write short names *)
 NES.Open This.Is.A.Long.Namespace.
 Print stuff.
+
+(* Not quite a name space yet *)
+Structure Default := { sort : Type; default : sort }.
+NES.Begin CS.
+  Global Canonical Structure nat_def := {| sort := nat; default := 46 |}.
+  Check @default _ : nat.
+NES.End CS.
+Fail Check nat_def.
+(* we want nat_def to live in the CS namespace, BUT
+   we want the canonical structure declaration to live outside the namespace *)
+Fail Check @default _ : nat.
+(* This behavior requires Libobject to be aware of the role played by
+   a module: if it is a namespace some "actions" have to be propagated upward *)
