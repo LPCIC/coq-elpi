86be7e17ce784cea737f4d917b478ef20fd1fe30
Enrico Tassi <Enrico.Tassi@Inria.fr>
2021-05-05 12:03:13 +0200

    restrict laws params less

--- apps/derive/theories/derive/lens_laws.v
+++ apps/derive/theories/derive/lens_laws.v
@@ -10,17 +10,17 @@ Definition law1_on {a c} (l : Lens a a c c) r := forall x,
   view l (set l x r) = x.
 Definition law1 {a c} (l : Lens a a c c) := forall r, law1_on l r.
 
-Definition law2_on {a c} (l : Lens a a c c) r := forall x y,
+Definition law2_on {a c d} (l : Lens a a c d) r := forall x y,
   set l y (set l x r) = set l y r.
-Definition law2 {a c} (l : Lens a a c c) := forall r, law2_on l r.
+Definition law2 {a c d} (l : Lens a a c d) := forall r, law2_on l r.
 
 Definition law3_on {a c} (l : Lens a a c c) r :=
   set l (view l r) r = r.
 Definition law3 {a c} (l : Lens a a c c) := forall r, law3_on l r.
-  
-Definition law4_on {a b d} (l1 : Lens a a b b) (l2 : Lens a a d d) r := forall x y,
+
+Definition law4_on {a b d e f} (l1 : Lens a a b d) (l2 : Lens a a e f) r := forall x y,
   set l1 x (set l2 y r) = set l2 y (set l1 x r).
-Definition law4 {a b d} (l1 : Lens a a b b) (l2 : Lens a a d d) := forall r, law4_on l1 l2 r.
+Definition law4 {a b d e f} (l1 : Lens a a b d) (l2 : Lens a a e f) := forall r, law4_on l1 l2 r.
 
 Register law1 as elpi.derive.lens.law1.
 Register law1_on as elpi.derive.lens.law1_on.
