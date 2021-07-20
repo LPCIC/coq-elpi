cb711722be8b844c64fc6ba646f23409c63fcc5b
Enrico Tassi <Enrico.Tassi@Inria.fr>
2021-07-20 12:45:19 +0200

    one stab at commands

--- examples/tutorial_coq_elpi_command.v
+++ examples/tutorial_coq_elpi_command.v
@@ -31,15 +31,12 @@ From elpi Require Import elpi.
      https://github.com/LPCIC/coq-elpi/blob/master/examples/tutorial_coq_elpi_HOAS.v
 
    - Defining commands
-   - arguments skeletons
+   - Arguments
+   - Processing raw arguments
    - Example: Synthesizing a term
+   - Example: Abstracting an inductive
    - Using DBs to store data across calls
-
-- accumulate, typecheck
-- arguments syntax and export
-- argument skeletons
-- data bases
-- attributes
+   - Attributes and Export
 
 *)
 
@@ -49,131 +46,316 @@ From elpi Require Import elpi.
 
 (**
 
-   To declare a command foo one usually
+   Lets declare a simple program, called "hello"
 
+*)
+Elpi Command hello.
+Elpi Accumulate lp:{{
 
+  % main is, well, the entry point
+  main Arguments :- coq.say "Hello" Arguments.
 
-   - built-in predicates (eg "coq.say") and first order terms
+}}.
+Elpi Typecheck.
+
+(*
+
+   The program declaration is made of 3 parts.
+   
+   The first one "Elpi Command hello." sets the current program to hello.
+   Since it is declared as a "Command" some code is loaded automatically:
+   - built-in predicates (eg "coq.say") and data types (eg Coq terms)
      https://github.com/LPCIC/coq-elpi/blob/master/coq-builtin.elpi
+   - some utilities, like "copy" or "whd1"
+     https://github.com/LPCIC/coq-elpi/blob/master/elpi/elpi-command-template.elpi
 
-   - higher order terms (eg Coq terms, detailed later in this tutorial)
-     https://github.com/LPCIC/coq-elpi/blob/master/elpi/coq-HOAS.elpi
 
-   - the user provided "<code>"
-
-   The more specific commands "Elpi Command" and "Elpi Tactic" take "<code>"
-   from predefined templates, respectively:
-
-   - https://github.com/LPCIC/coq-elpi/blob/master/elpi/elpi-command-template.elpi
-   - https://github.com/LPCIC/coq-elpi/blob/master/elpi/elpi-tactic-template.elpi
-
-   The "Elpi Accumulate ..." family of commands lets one accumulate clauses
+   The second one "Elpi Accumulate ..." loads some extra code.
+   The "Elpi Accumulate ..." family of commands lets one accumulate code
    taken from:
-
    - verbatim text "Elpi Accumulate lp:{{ <code> }}"
    - source files "Elpi Accumulate File <path>"
    - data bases (Db) "Elpi Accumulate Db <name>"
-
    Accumulating code via inline text of file is equivalent, the AST of code
    is stored in the .vo file (the external file does not need to be installed).
    We postpone the description of data bases to a dedicated section.
 
    Once all the code is accumulated "Elpi Typecheck" verifies that the
    code does not contain the most frequent kind of mistakes. This command
-   considers some mistakes minor and only warns about them. We recommend
-   passing "-w +elpi.typecheck" to coqc in final products.
+   considers some mistakes minor and only warns about them. You can
+   pass "-w +elpi.typecheck" to coqc to turn these warnings into errors.
 
-   Finally, "Elpi Export" can make you command avilable (without having
-   to prefix its name by Elpi).
-
-   The entry point of a command is the "main" predicate.
+   We can now run our program!
 
 *)
 
-Elpi Command tutorial.
-Elpi Accumulate lp:{{
+Elpi hello "world!".
 
-  main Arguments :- coq.say "Hello" Arguments.
+(*
 
-}}.
-Elpi Typecheck.
+   You should see
 
-Elpi tutorial "world!".
+     Hello [str world!]
 
-Elpi Export tutorial.
+   The  string "world!" we passed to the command is received by the code
+   as (str "world!").
 
-tutorial "world!".
-
-
-
-
-(**
-    Elpi programs, like Dbs, are open ended: they can
-    be extended later on by accumulating extra code. The same consideration
-    about named clauses as anchor point and the :before attribute apply.
-*)
-Elpi Accumulate tutorial lp:{{
-
-  main _ :- coq.say "usage: tutorial [name]".
-
-}}.
-Elpi tutorial "too" "many" "args".
-
-
-(**
-  Arguments can either be numbers, strings (no double quote needed if the string
-  happens to be a qualified name), terms (always between parentheses) or
-  declarations (as Record, Inductive or Definition).
 *)
 
-Elpi Command show_arguments.
-Elpi Accumulate lp:{{
+(** ------------------------- Arguments ---------------------------- *)
 
-  main Arguments :-
-    coq.say "Arguments: " Arguments.
+Elpi hello 46.
+Elpi hello there.
 
-}}.
-Elpi Typecheck.
-Elpi show_arguments "argument1" argum.ent2 3 (1 = 2).
+(*
+
+  This time we passed to the command a number and an identifier, and we get
+
+    Hello [int 46]
+    Hello [str there]
+
+  Identifiers are received as strings, and can contain dots, but no spaces
+*)
+
+Elpi hello my friend.
+Elpi hello this.is.a.qualified.name.
+
+(*
+
+  Indeed the first invocation passes two arguments, of type string, while
+  the second a single one, again a string containing dots.
+
+    Hello [str my, str friend]
+    Hello [str this.is.a.qualified.name]
+
+  There are a few more types of arguments a command can receive:
+  - terms, delimited by "(" and ")"
+  - toplevel declarations, like "Inductive ..", "Definition ..", etc..
+    which are introduced by their characterizing keyword.
+
+  Lets try with a term
+
+*)
+
+Elpi hello (0 = 1).
 
 (**
-  Terms are passed "raw", in the sense that no elaboration has been
+
+  We get
+
+    Hello 
+      [trm (app [global (indt «eq»),
+                 X0,
+                 global (indc «O»), 
+                 app [global (indc «S»), global (indc «O»)]])]
+
+  Terms are received "raw", in the sense that no elaboration has been
   performed. In the example above the type argument to "eq" has not
   been synthesized to be "nat". As we see later, the "coq.typecheck" API
   can be used to satisfy typing constraints.
+
 *)
 
-(** Commands can be exported to the usual name space of command with
-    Elpi Export *)
+Elpi hello Definition test := 0 = 1.
+Elpi hello Record test := { f1 : nat; f2 : f1 = 1 }.
 
-Elpi Export show_arguments.
+(*
 
-show_arguments 1 "2" (3).
+  Global declarations are received raw as well
 
-(**
-   Elpi comes with a type checker. It is invoked any time
-   a query is run by hand via "Elpi Query", or when invoked via
-   "Elpi Typecheck".
-   It is not run when a "Command" or "Tactic" in invoked via its main
-   entry point.
+    Hello [const-decl test 
+      (some (app [global (indt «eq»), X0, global (indc «O»), 
+                   app [global (indc «S»), global (indc «O»)]]))
+      (arity X1)]
+    
+    Hello [indt-decl
+      (record test (sort (typ X0)) Build_test 
+    	  (field [coercion ff, canonical tt] f1 (global (indt «nat»)) c0 \
+         field [coercion ff, canonical tt] f2 
+           (app
+             [global (indt «eq»), X1 c0, c0, 
+              app [global (indc «S»), global (indc «O»)]]) c1 \
+         end-record))]
+
+  See the "argument" data type in 
+    https://github.com/LPCIC/coq-elpi/blob/master/coq-builtin.elpi
+  for a detailed decription of all the arguments a command can receive.
+
 *)
-Elpi Command illtyped.
+
+(** -------------------- Processing raw arguments --------------------- *)
+
+(*
+    There are two ways to process term arguments.
+    
+*)
+
+Elpi Command check_arg.
 Elpi Accumulate lp:{{
-  pred test o:bool.
-  test (tt ^ "3").
+
+  main [trm T] :-
+    std.assert-ok! (coq.typecheck T Ty) "argument illtyped",
+    coq.say "The type of" T "is" Ty.
+
 }}.
-Fail Elpi Typecheck illtyped.
+Elpi Typecheck.
 
+Elpi check_arg (1 = 0).
+Fail Elpi check_arg (1 = true).
 
-(** --------------------------------------------------------------- *)
+(*
 
+  The command "check_arg" received a term T and type checks it, then it
+  prints the term and its type.
 
+  The "coq.typecheck" API has 3 arguments, a term, its type and a diagnostic
+  which can either be "ok" or "(error Message)". The "std.assert-ok!" combinator
+  checks if the diagnostic is ok, and if not it prints the error message and
+  bails out.
+  
+  The first invocation succeeds
+  
+    The type of 
+      app [global (indt «eq»), global (indt «nat»), 
+           app [global (indc «S»), global (indc «O»)],
+           global (indc «O»)]
+    is 
+      sort prop
 
+  The second one fails
+
+    argument illtyped: Illegal application: 
+    The term "@eq" of type "forall A : Type, A -> A -> Prop"
+    cannot be applied to the terms
+     "nat" : "Type"
+     "1" : "nat"
+     "true" : "bool"
+    The 3rd term has type "bool" which should be coercible to "nat".
+
+*)
+
+Coercion bool2nat (b : bool) := if b then 1 else 0.
+Fail Elpi check_arg (1 = true).
+Check (1 = true).
+
+(*
+  The command still fails even if we told Coq how to inject booleans values
+  into the natural numbers. Indeed the Check commands works.
+
+  The "coq.typecheck" API modifies the term in place, it can assign
+  implicit arguments (like the type parameter of eq) but cannot modify the
+  structure of th term. To do so, one has to use the "coq.elaborate-skeleton"
+  API
+*)
+
+Elpi Command elaborate_arg.
+Elpi Accumulate lp:{{
+
+  main [trm T] :-
+    std.assert-ok! (coq.elaborate-skeleton T Ty T1) "illtyped arg",
+    coq.say "T=" T,
+    coq.say "T1=" T1,
+    coq.say "Ty=" Ty.
+    
+}}.
+Elpi Typecheck.
+
+Elpi elaborate_arg (1 = true).
+
+(* 
+
+   Remark how T is not touched by the API, and how T1 is a copy where
+   the hole after eq was synthesized and the value true injected to nat
+   by using bool2nat.
+ 
+     T=  app [global (indt «eq»), X0,
+              app [global (indc «S»), global (indc «O»)], 
+              global (indc «true»)]
+     T1= app [global (indt «eq»), global (indt «nat»), 
+              app [global (indc «S»), global (indc «O»)], 
+              app [global (const «bool2nat»), global (indc «true»)]]
+     Ty= sort prop
+
+   It is also possible to manipulate term arguments before typechecking
+   them, but note that all the considerations on Holes in the tutorial about
+   the HOAS representation of Coq terms apply here. 
+*)
+
+(** -------------------- Example: Synthesizing a term ---------------------- *)
+
+(**
+   Synthesizing a term typically involves reading an existing declaration
+   and writing a new one. The relevant APIs are in the "coq.env.*" namespace
+   and are named after the global refence they manipulate, eg "coq.env.const"
+   for reading and "coq.env.add-const" for writing.
+
+   Here we implement a little command that given an inductive type
+   generates a term of type nat whose value is the number of constructors
+   of the given inductive type.
+*)
+
+Elpi Command constructors_num.
+
+Elpi Accumulate lp:{{
+
+  pred int->nat i:int, o:term.
+  int->nat 0 {{ 0 }}.
+  int->nat N {{ S lp:X }} :- M is N - 1, int->nat M X.
+
+  main [str IndName, str Name] :-
+    coq.locate IndName (indt GR),
+    coq.env.indt GR _ _ _ _ Kn _,         % get the names of the constructors
+    std.length Kn N,                      % count them
+    int->nat N Nnat,                      % turn the integer into a nat
+    coq.env.add-const Name Nnat _ _ _.    % save it
+
+}}.
+Elpi Typecheck.
+
+Elpi constructors_num bool nK_bool.
+Print nK_bool. (* number of constructor of "bool" *)
+Elpi constructors_num False nK_False.
+Print nK_False.
+
+(** -------------------- Example: Abstracting an inductive --------------- *)
+
+Elpi Command abstract.
+
+Elpi Accumulate lp:{{
+
+  pred prime i:string, o:string.
+  prime S S1 :- S1 is S ^ "'".
+
+  main [str Ind, trm Param] :-
+    
+    std.assert-ok! (coq.elaborate-skeleton Param PTy P) "illtyped parameter",
+    
+    coq.locate Ind (indt I),
+    coq.env.indt-decl I Decl,
+    NewDecl = parameter "A" explicit PTy Decl',
+    (pi a\ copy P a => copy-indt-decl Decl (Decl' a)),
+    coq.rename-indt-decl (=) prime prime NewDecl DeclRenamed,
+    std.assert-ok! (coq.typecheck-indt-decl DeclRenamed) "can't be abstracted",
+    coq.env.add-indt DeclRenamed _.
+
+}}.
+Elpi Typecheck.
+
+Inductive tree := leaf | node : tree -> option nat -> tree -> tree.
+
+Elpi abstract tree (option nat).
+Print tree'.
+
+(*
+Inductive tree' (A : Set) : Set :=
+	leaf' : tree' A | node' : tree' A -> A -> tree' A -> tree' A
+*)
+
+(** ------------------ Using DBs to store data across calls ---------------- *)
 
 (**
 
-
-   A "Db" can be create with the command:
+   A Db can be create with the command:
 
    - "Elpi Db <name> lp:{{ <code> }}"
 
@@ -205,27 +387,18 @@ Elpi Db age.db lp:{{ % We like Db names to end in a .db suffix
    Let's define a Command that makes use of a Db.
 *)
 
-Elpi Command tutorial.
-Elpi Accumulate Db age.db.
+Elpi Command age.
+Elpi Accumulate Db age.db.  (* we accumulate the Db *)
 Elpi Accumulate lp:{{
 
-  main []         :- coq.say "hello world".
-
-  main [str Name] :- coq.say "hello" Name ", you are" {age Name}.
+  main [str Name] :-
+    age Name A,
+    coq.say Name "is" A "years old".
 
 }}.
-Elpi Typecheck. (* We will detail later what this is for *)
+Elpi Typecheck. 
 
-(** The entry point for commands is "main" that receives a list of
-    arguments. See https://github.com/LPCIC/coq-elpi/blob/master/coq-builtin.elpi
-    for their constructors/types. Here we use "str" that carries a string.
-
-    Elpi provides a syntax to call (query) the standard entry point of commands
-    (and later tactics).
-*)
-
-Elpi tutorial.           (** Elpi Query tutorial lp:{{ main [] }} *)
-Fail Elpi tutorial bob.  (** Elpi Query tutorial lp:{{ main [str "bob"] }} *)
+Fail Elpi age bob.
 
 (**
    Let's put some data in the Db. Given that the Db contains a catch-all clause,
@@ -238,43 +411,131 @@ Elpi Accumulate age.db lp:{{
 
 }}.
 
-Elpi tutorial bob.
+Elpi age bob.
 
+(*
 
-(** -------------------- Example: Synthesizing a term ---------------------- *)
+  Elpi programs can extend a Db as well
 
-(**
-   Synthesizing a term typically involves reading an existing declaration
-   and writing a new one. The relevant APIs are in the "coq.env.*" namespace
-   and are named after the global refence they manipulate, eg "coq.env.const"
-   for reading and "coq.env.add-const" for writing.
-
-   Here we implement a little command that given an inductive type
-   generates a term of type nat whose value is the number of constructors
-   of the given inductive type.
 *)
 
-Elpi Command Constructors_num.
-
+Elpi Command set_age.
+Elpi Accumulate Db age.db.
 Elpi Accumulate lp:{{
-
-  pred int->nat i:int, o:term.
-  int->nat 0 {{ 0 }}.
-  int->nat N {{ S lp:X }} :- M is N - 1, int->nat M X.
-
-  main [str IndName, str Name] :-
-    coq.locate IndName (indt GR),
-    coq.env.indt GR _ _ _ _ Kn _,         % get the names of the constructors
-    std.length Kn N,                      % count them
-    int->nat N Nnat,                      % turn the integer into a nat
-    coq.env.add-const Name Nnat _ _ _. % save it
+  main [str Name, int Age] :-
+    coq.elpi.accumulate _ "age.db" (clause _ (before "age.fail") (age Name Age)).
+  
 }}.
 Elpi Typecheck.
-Elpi Export Constructors_num.
 
-Constructors_num bool nK_bool.
-Print nK_bool. (* number of constructor of "bool" *)
-Constructors_num False nK_False.
-Print nK_False.
+Elpi set_age "alice" 21.
+Elpi age "alice".
 
 
+(** ----------------------- Attributes and Export -------------------------- *)
+
+(*
+
+   Elpi programs can be prefixed with attributes, like #[local].
+   Attributes are not passed as arguments but rather as a clause.
+   
+*)
+
+Elpi Command attr.
+Elpi Accumulate lp:{{
+
+  main _ :-
+    attributes A,
+    coq.say A.
+
+}}.
+
+#[this, more(stuff="33")] Elpi attr.
+
+(*
+[attribute elpi.loc (leaf-loc File "(stdin)", line 3, column 35, characters 33-37:), 
+ attribute this (leaf-str ), 
+ attribute more (node [attribute stuff (leaf-str 33)])]
+*)
+
+Elpi Command parse_attr.
+Elpi Accumulate lp:{{
+
+  pred some-code.
+  some-code :- get-option "more.stuff" N, get-option "this" B, coq.say N B.
+
+  main _ :-
+    attributes A,
+    coq.parse-attributes A [
+      att "this" bool,
+      att "more.stuff" int,
+    ] Opts,
+    coq.say Opts,
+    Opts => some-code.
+
+}}.
+
+#[this, more(stuff="33")] Elpi parse_attr.
+Fail #[unknown] Elpi parse_attr.
+(*
+33 tt
+The command has indeed failed with message:
+Attribute unknown is not supported
+*)
+
+(*
+
+  Elpi programs can be exported as regular Coq commands, so that the
+  final user does not need to type Elpi to invoke them.
+
+*)
+
+Elpi Command say.
+Elpi Accumulate lp:{{ main [str S] :- coq.say S. }}.
+Elpi Typecheck.
+Elpi Export say.
+
+say "That is all folks!".
+
+(*
+
+  Not quite. Coq offers no equivalent of Tactic Notation for commands.
+  Still Elpi commands accept any symbol or keyword as strings.
+
+*)
+
+Elpi Command go.
+Elpi Accumulate lp:{{
+  main [str Src, str "=>", str Tgt, str "/", str F] :- !,
+    coq.say "going from" Src "to" Tgt "via" F.
+  main _ :- coq.error "Parse error! Use: go <from> => <to> / <via>".
+}}.
+Elpi Typecheck.
+Elpi Export go.
+
+go source => target / plane.
+Fail go nowhere.
+
+(* 
+ going from source to target via plane
+The command has indeed failed with message:
+Parse error! Use: go <from> => <to> / <via>
+
+
+  Last, (good) Elpi programs should fail reporting intellegible error messages,
+  as the previous one. If they just fail, they produce the following generic
+  error.
+
+*)
+Elpi Command bad.
+Elpi Accumulate lp:{{ main []. }}.
+Elpi Typecheck.
+Elpi Export bad.
+
+Fail bad 1.
+
+(*
+The command has indeed failed with message:
+The elpi command bad failed without giving a specific error message. Please
+report this inconvenience to the authors of the program.
+*)
