4d449fbe1e312b52af567187d87a63d33c5d06a0
Enrico Tassi <Enrico.Tassi@Inria.fr>
2022-07-25 14:24:10 +0200

    xxx

--- .nix/config.nix
+++ .nix/config.nix
@@ -7,6 +7,7 @@
     "coq-8.16".coqPackages = {
       coq.override.version = "8.16";
       hierarchy-builder.override.version = "master";
+      hierarchy-builder-shim.job = false;
       mathcomp.override.version = "hierarchy-builder";
       mathcomp.job = true;
       odd-order.override.version = "hirarchy-builder";
