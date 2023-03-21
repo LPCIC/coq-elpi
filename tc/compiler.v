2dbdb3f32215119edf0548ca4570e1322612ef30
Fissore Davide <fisso9900@gmail.com>
2023-03-21 13:44:46 +0100

    Update compiler to add only "simpl" instances

--- tc/compiler.v
+++ tc/compiler.v
@@ -16,24 +16,39 @@ Elpi Accumulate lp:{{
   pred is-in-path i:string, i:gref. 
   is-in-path Path GR :- 
     std.mem {coq.gref->path GR} Path.
-  
+
+  % returns if GR is not a quantified instance
+  pred is-simpl i:gref. 
+  is-simpl GR :- 
+    coq.env.typeof GR (app _).
+
   pred add-inst->db i:gref.
   add-inst->db Inst :-
     coq.env.typeof Inst Ty,
     coq.say Inst,
     coq.elpi.accumulate _ "tc.db" (clause _ _ (tc Ty (global Inst))).
 
+  pred get-inst-by-tc-name i:string, o:list gref.
+  get-inst-by-tc-name ClassName GRL:-
+    coq.TC.db-for {coq.locate ClassName} Inst,
+    std.map Inst inst->gref GRL.
+
   % Look for the instances of ClassName 
   % that are located in Path. The results are 
   % added to tc.db
   % pred find-tc i:string, i:string, i:string, i:(term -> term -> prop).
-  pred find-tc i:string, i:string.
-  find-tc ClassName Path :-
-    coq.TC.db-for {coq.locate ClassName} Inst,
-    std.map Inst inst->gref GRL,
-    std.filter GRL (is-in-path Path) InstInPath,
+  pred add-path i:string, i:string.
+  add-path ClassName Path :-
+    std.filter {get-inst-by-tc-name ClassName} (is-in-path Path) InstInPath,
     std.forall InstInPath add-inst->db.
 
-  main [str ClassName, str Path]:- find-tc ClassName Path.
+  % Add tc if not quantified
+  pred add-simpl i:string.
+  add-simpl ClassName :-
+    std.filter {get-inst-by-tc-name ClassName} (is-simpl) InstInPath,
+    std.forall InstInPath add-inst->db.
+
+  main [str ClassName, str ""] :- !, add-simpl ClassName.
+  main [str ClassName, str Path]:- add-path ClassName Path.
 }}.
 Elpi Typecheck.
