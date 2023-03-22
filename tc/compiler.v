326932e2d08c6b0ad85c7c9590219848262d6171
Davide Fissore <fisso9900@gmail.com>
2023-03-22 16:25:00 +0100

    Updata for the insertion of simpl Instances in db

--- tc/compiler.v
+++ tc/compiler.v
@@ -4,12 +4,28 @@ From elpi.tc Require Import instances_db.
 Elpi Command add_inst_by_path.
 
 Elpi Accumulate Db tc.db.
+
 Elpi Accumulate lp:{{
+  % In this Accumulate, we have auxiliary functions
+  % to add the instances into the DB
+
+  pred is-class i:term.
+  is-class (global C) :- coq.TC.class? C.
+
+  pred get-inst-by-tc-name i:string, o:list gref.
+  get-inst-by-tc-name ClassName GRL:-
+    coq.TC.db-for {coq.locate ClassName} Inst,
+    std.map Inst inst->gref GRL.
 
   % takes a tc-instance and return the gref
   pred inst->gref i:tc-instance, o:gref.
   inst->gref Inst Res :-  
     tc-instance Res _ = Inst.
+
+  pred compile i:term, i:term, o:prop.
+  compile (prod _ _ F) I (pi p\ C p) :-
+    pi p\ compile (F p) (app[I, p]) (C p).
+  compile Ty I (tc Ty I).
 
   % takes a Path and a GR and returns if 
   % the GR is located in Path
@@ -17,24 +33,42 @@ Elpi Accumulate lp:{{
   is-in-path Path GR :- 
     std.mem {coq.gref->path GR} Path.
 
-  % returns if GR is not a quantified instance
-  pred is-simpl i:gref. 
-  is-simpl GR :- 
-    coq.env.typeof GR (app _).
-
   pred add-inst->db i:gref.
-  :if "get added classes"
-  add-inst->db Inst :- coq.say Inst.
-
+  :if "debug"
+  add-inst->db Inst :- coq.say "Adding instance:" Inst, fail.
   add-inst->db Inst :-
     coq.env.typeof Inst Ty,
-    coq.elpi.accumulate _ "tc.db" (clause _ _ (tc Ty (global Inst))).
+    compile Ty (global Inst) C,
+    coq.elpi.accumulate _ "tc.db" (clause _ _ C).
+}}.
+Elpi Typecheck.
 
-  pred get-inst-by-tc-name i:string, o:list gref.
-  get-inst-by-tc-name ClassName GRL:-
-    coq.TC.db-for {coq.locate ClassName} Inst,
-    std.map Inst inst->gref GRL.
+Elpi Accumulate lp:{{
+  pred is-simpl-term i:term, o:int.
+  is-simpl-term (prod _ T E) Cmp :- !,
+    (pi x\ is-simpl-term (E x) Tot1),
+    Cmp is {is-simpl-term T} + Tot1, Cmp < 2.
+  is-simpl-term (app E) Cmp :- !,
+    std.filter E is-class L,
+    Cmp = {std.length L}, Cmp < 2.
+  is-simpl-term _ 0.
 
+  % coq term dependencies -> pour simplifier is-simpl-term
+
+  % returns if GR is not a quantified instance
+  pred is-simpl-gref i:gref. 
+  is-simpl-gref GR :- 
+    is-simpl-term {coq.env.typeof GR} _.
+
+  % Add tc if not quantified
+  pred add-simpl i:string.
+  add-simpl ClassName :-
+    std.filter {get-inst-by-tc-name ClassName} (is-simpl-gref) SimplInst,
+    std.forall SimplInst add-inst->db.
+}}.
+Elpi Typecheck.
+
+Elpi Accumulate lp:{{
   % Look for the instances of ClassName 
   % that are located in Path. The results are 
   % added to tc.db
@@ -43,14 +77,28 @@ Elpi Accumulate lp:{{
   add-path ClassName Path :-
     std.filter {get-inst-by-tc-name ClassName} (is-in-path Path) InstInPath,
     std.forall InstInPath add-inst->db.
+}}.
+Elpi Typecheck.
 
-  % Add tc if not quantified
-  pred add-simpl i:string.
-  add-simpl ClassName :-
-    std.filter {get-inst-by-tc-name ClassName} (is-simpl) InstInPath,
-    std.forall InstInPath add-inst->db.
+Elpi Accumulate lp:{{
 
-  main [str ClassName, str ""] :- !, add-simpl ClassName.
-  main [str ClassName, str Path]:- add-path ClassName Path.
+  kind enum type.
+  type all, instance  string -> enum.
+  type path  string -> string -> enum.
+
+  pred parse i:list argument, o:enum.
+  parse [str ClassName] (all ClassName).
+  parse [str InstName, str "instance"] (instance InstName).
+  parse [str ClassName, str Path] (path ClassName Path).
+
+  pred run-command i:enum.
+  :if "debug"
+  run-command A :- coq.say A, fail.
+  run-command (all ClassName) :- add-simpl ClassName.
+  run-command (instance InstName) :- add-inst->db {coq.locate InstName}.
+  run-command (path ClassName Path):- add-path ClassName Path.
+
+  % The main of the Command
+  main Arguments :- parse Arguments Res, run-command Res.
 }}.
 Elpi Typecheck.
--- tc/instances_db.v
+++ tc/instances_db.v
@@ -14,6 +14,8 @@ Elpi Accumulate lp:{{
   msolve L N :-
     std.rev L LR, coq.ltac.all (coq.ltac.open solve) LR N.
 
+  :if "debug"
+  solve A _ :- coq.say "Solving", fail.
   solve (goal _ _ Ty Sol _ as G) GL :- var Sol,
     (if (tc Ty X)  (refine X G GL ; coq.say "illtyped solution:" {coq.term->string X}) (GL = [seal G])).
 }}.
