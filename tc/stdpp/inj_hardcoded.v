ab2e45b37c817179bbffad2f5a97662cdd6ff33a
Davide Fissore <fisso9900@gmail.com>
2023-03-22 16:25:00 +0100

    Updata for the insertion of simpl Instances in db

--- tc/stdpp/inj_hardcoded.v
+++ tc/stdpp/inj_hardcoded.v
@@ -3,63 +3,44 @@ From stdpp Require Export base.
 From elpi.tc.stdpp Require Export inj_fun_ex.
 From elpi.tc Require Import compiler.
 
-(* Set Warnings "+elpi". *)
+Set Warnings "+elpi".
 
 (* Unset Typeclass Resolution For Conversion. *)
 
 (* The third param of inj cannot be flexible *)
 
-Elpi Tactic HS_Inj.
-Elpi Accumulate Db tc.db.
-
-Elpi Accumulate lp:{{
-  msolve L N :-
-    std.rev L LR, coq.ltac.all (coq.ltac.open solve) LR N.
-
-  solve (goal _ _ Ty Sol _ as G) GL :- var Sol,
-    (if (tc Ty X)  (refine X G GL ; coq.say "illtyped solution:" {coq.term->string X}) (GL = [seal G])).
-}}.
-Elpi Typecheck.
-
-Elpi Accumulate lp:{{
+Elpi Accumulate tc.db lp:{{
   tc {{Inj _ _ lp:T}} _ :- var T, !, coq.say "Inj: the instance function cannot be flexible", fail.
 }}.
-Elpi Typecheck.
+Elpi Typecheck TC_check.
 
 (* Composition of injective functions  *)
-Elpi Accumulate lp:{{
+Elpi Accumulate tc.db lp:{{
   tc {{@Inj lp:A lp:C lp:R1 lp:R3 (@compose lp:A lp:B lp:C lp:G lp:F) }} Sol :-
     tc {{@Inj lp:A lp:B lp:R1 lp:R2 lp:F}} InjF,
     tc {{@Inj lp:B lp:C lp:R2 lp:R3 lp:G}} InjG,
     Sol = {{@compose_inj lp:A lp:B lp:C lp:R1 lp:R2 lp:R3 lp:F lp:G lp:InjF lp:InjG }}. 
 }}. 
-Elpi Typecheck.
+Elpi Typecheck TC_check.
 
 (* Prod map is inj *)
-Elpi Accumulate lp:{{
+Elpi Accumulate tc.db lp:{{
    tc {{Inj eq eq (prod_map lp:F lp:G) }} Sol :-
     tc {{Inj eq eq lp:F}} InjF,
     tc {{Inj eq eq lp:G}} InjG,
     Sol = {{prod_map_inj lp:F lp:G lp:InjF lp:InjG }}.
 }}.
-Elpi Typecheck.
+Elpi Typecheck TC_check.
 
-(* inr and inl are injective  *)
-Elpi Accumulate lp:{{
-  tc {{Inj eq eq inr}} {{inr_inj}}.
-  tc {{Inj eq eq inl}} {{inl_inj}}.
-}}.
-Elpi Typecheck.
-
-Elpi Accumulate lp:{{
+Elpi Accumulate tc.db lp:{{
   tc {{Inj eq eq (sum_map lp:F lp:G)}} Sol :- 
     coq.say "test",
     Sol = {{sum_map_inj lp:F lp:G lp:InjF lp:InjG}},
     tc {{Inj eq eq lp:F}} InjF, 
     tc {{Inj eq eq lp:G}} InjG.
 }}.
-Elpi Typecheck.
+Elpi Typecheck TC_check.
 
+Elpi add_inst_by_path "Inj".
 
-(* Elpi Debug "get added classes". *)
-Elpi add_inst_by_path "Inj" "inj_fun_ex".
+Elpi Override TC TC_check.
--- tc/stdpp/inj_hardcoded_test.v
+++ tc/stdpp/inj_hardcoded_test.v
@@ -2,14 +2,14 @@ From elpi.tc.stdpp Require Import inj_hardcoded.
 From elpi Require Import elpi.
 (* Elpi Bound Steps 1000. *)
 
-Elpi Override TC HS_Inj.
+Elpi Debug "debug".
 
 Check (_ : Inj _ _ g).
+Check (_ : Inj _ _ inr).
 
 Check (_ : Inj eq1 eq3 (@compose nat nat nat g f)).
 Check (_ : Inj _ _ (compose g g)).
 
-Check (_ : Inj _ _ inr).
 Check (_ : Inj eq eq (prod_map f f)).
 
 Check (_ : Inj _ _ (prod_map g (compose f f))).
