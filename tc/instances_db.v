713403028ecec499908dac2f0f3c5326a658b3c9
Davide Fissore <fisso9900@gmail.com>
2023-03-23 17:29:30 +0100

    Add modes to compiler (list of length 0 or 1)

--- tc/instances_db.v
+++ tc/instances_db.v
@@ -3,14 +3,9 @@ From elpi Require Import elpi.
 Elpi Db tc.db lp:{{
   pred tc o:term, o:term.
 
+  :name "hook"
   % T cannot be a free variable
   tc T _ :- var T, !, coq.say "fail on flexible function", fail.
-
-  tc (app [global (const C) | TL]) _ :- 
-    var {std.last TL}, !,  
-    coq.say "Last param of " C "can't be flexible", 
-    fail.
-  tc (prod _ _ X) _ :- pi x\ tc (X x) _.
 }}.
 
 
@@ -23,7 +18,7 @@ Elpi Accumulate lp:{{
   :if "debug"
   solve A _ :- coq.say "Solving", fail.
   solve (goal _ _ Ty Sol _ as G) GL :- var Sol,
-    (if (tc Ty X)  (refine X G GL ; coq.say "illtyped solution:" {coq.term->string X}) (GL = [seal G])).
+    if (tc Ty X) (refine X G GL ; coq.say "illtyped solution:" {coq.term->string X}) (GL = [seal G]).
 }}.
 Elpi Typecheck.
 
