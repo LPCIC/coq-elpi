18d90f48d0d8b6aa40ab0ac99f4da710c8467ef7
Davide Fissore <fisso9900@gmail.com>
2023-03-24 14:05:32 +0100

    Compiler warning if inst has multiple class dep + ignoreClasses

--- tc/stdpp/inj_hardcoded.v
+++ tc/stdpp/inj_hardcoded.v
@@ -3,19 +3,16 @@ From stdpp Require Export base.
 From elpi.tc.stdpp Require Export inj_fun_ex.
 From elpi.tc Require Import compiler.
 
-Set Warnings "+elpi".
-
-(* Elpi add_instances Inj. *)
-
 (* Global Hint Mode Inj - - ! ! -: typeclass_instances.
 Global Hint Mode Inj - - ! ! !: typeclass_instances. *)
 
+(* Elpi Debug "debug". *)
 Elpi Accumulate TC_check Db tc.db.
 
-(* Elpi add_instances "Inj" exclude isInjf_eq. *)
-Elpi add_instances "Inj".
-(* Elpi add_instances "Inj" exclude "compose_inj". *)
-(* Elpi add_instances "instances" "compose_inj" "isInjf_eq" "isInjg_eq". *)
-(* Elpi add_instances "instances" "inr_inj". *)
+(* Elpi add_instances Inj ignoreClasses Inj2 Equiv. *)
+(* Elpi add_instances Inj ignoreInstances isInjf_eq. *)
+(* Elpi add_instances Inj ignoreInstances compose_inj. *)
+(* Elpi add_instances instances compose_inj isInjf_eq isInjg_eq. *)
+(* Elpi add_instances instances inr_inj. *)
 
 Elpi Override TC TC_check.
