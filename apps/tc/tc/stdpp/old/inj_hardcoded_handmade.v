6cce92b605d802c9f31336fec289e8f463f48d85
Davide Fissore <fisso9900@gmail.com>
2023-03-27 14:15:10 +0200

    update coqProject

--- apps/tc/tc/stdpp/old/inj_hardcoded_handmade.v
+++ apps/tc/tc/stdpp/old/inj_hardcoded_handmade.v
@@ -1,7 +1,7 @@
 From elpi Require Import elpi.
 From stdpp Require Export base.
-From elpi.tc.stdpp Require Export inj_fun_ex.
-From elpi.tc Require Import compiler.
+From elpi.apps Require Export tc.stdpp.inj_fun_ex.
+From elpi.apps Require Import tc.compiler.
 
 Set Warnings "+elpi".
 
