cc7ed1c63ce34f52d2e5853506efa802cb954d7c
Davide Fissore <fisso9900@gmail.com>
2023-03-28 11:52:47 +0200

    Refactor .elpi + Add Proof Ctx in current env

--- apps/tc/tc/compiler.v
+++ apps/tc/tc/compiler.v
@@ -2,12 +2,14 @@ From elpi Require Import elpi.
 From elpi.apps Require Import tc.instances_db.
 From elpi.apps.tc Extra Dependency "compiler.elpi" as compiler.
 From elpi.apps.tc Extra Dependency "parserAPI.elpi" as parserAPI.
+From elpi.apps.tc Extra Dependency "modes.elpi" as modes.
 
 Elpi Command add_instances.
 
 Set Warnings "+elpi".
 
 Elpi Accumulate Db tc.db.
+Elpi Accumulate File modes.
 Elpi Accumulate File compiler.
 Elpi Accumulate File parserAPI.
 Elpi Typecheck.
