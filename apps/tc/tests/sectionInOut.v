fc37973bea4076a0da1cf9d30e9935cc9c3ff599
Davide Fissore <fisso9900@gmail.com>
2023-04-18 21:51:43 +0200

    No more bug on end-env-section + Add InstFun to index
    todo make the index on the InstFun effective

---
+++ apps/tc/tests/sectionInOut.v
@@ -0,0 +1,55 @@
+From elpi.apps.tc Require Import compiler.
+From elpi.apps.tc Extra Dependency "base.elpi" as base.
+
+Elpi Command len_test.
+Elpi Accumulate Db tc.db.
+Elpi Accumulate File base.
+Elpi Accumulate lp:{{
+  main [int Len] :-
+    std.findall (instance _ _ _) Insts,
+    std.map Insts (x\r\ instance _ r _ = x) R,
+    % coq.say R,
+    std.assert! ({std.length R} = Len) 
+      "Unexpected number of instances",
+    std.forall R (x\ sigma L\
+      std.assert! (count R x L, L = 1) "Duplicates in instances"). 
+}}.
+Elpi Typecheck.
+
+Class Eqb A:= eqb: A -> A -> bool.
+Global Instance eqA : Eqb unit := { eqb x y := true }.
+
+Elpi add_instances Eqb.
+
+Elpi len_test 1.
+
+Section A.
+  Context (A : Type).
+  Global Instance eqB : Eqb bool := { eqb x y := if x then y else negb y }.
+  Elpi add_instances Eqb.
+  Elpi len_test 2.
+  
+  Global Instance eqC : Eqb A := {eqb _ _ := true}.
+  Elpi add_instances Eqb.
+  Elpi len_test 3.
+
+  Section B.
+    Context (B : Type).
+    Global Instance eqD : Eqb B := {eqb _ _ := true}.
+    Elpi add_instances Eqb.
+    Elpi len_test 4.
+  Elpi myEnd.
+
+  Elpi len_test 4.
+
+Elpi myEnd.
+
+(* 
+  TODO : the order is not mantained - 
+  the list of instances is A B D C and not A B C D.
+  Uncomment line 11 to see this behavior
+*)
+Elpi len_test 4.
+
+
+
