b17d9164d0628bc5fb95e5424ffe948753e9db00
Davide Fissore <fisso9900@gmail.com>
2023-06-22 23:19:24 +0200

    Add tests
    Co-Authored-By: Enrico Tassi <gares@users.noreply.github.com>

--- tests/test_API_new_pred.v
+++ tests/test_API_new_pred.v
@@ -1,45 +1,54 @@
 From elpi Require Import elpi.
 
 Elpi Db search.db lp:{{
+  pred p i:int.
+  p 1.
 
-pred p i:int.
-p 1.
-
+  pred unwrap i:list argument, o:list any.
+  unwrap [] [].
+  unwrap [A | As] [B | Bs] :-
+    (A = int B; A = str B; A = trm B),
+    unwrap As Bs.
 }}.
 
 Elpi Command search.
 Elpi Accumulate Db search.db.
 Elpi Accumulate lp:{{
-main [str P, int I] :-
-  coq.elpi.predicate P [I] Q,
-  Q.
-main [str P] :-
-  coq.elpi.predicate P [I] Q,
-  Q,
-  coq.say "I=" I.
+  main [str "output", str P | L_args] :-
+    unwrap L_args L,
+    std.append L [X_] L1,
+    coq.elpi.predicate P L1 Q, Q,
+    coq.say "I=" {std.last L1}.
+  main [str P | L_args] :-
+    unwrap L_args L,
+    coq.elpi.predicate P L Q, Q.
 }}.
 Elpi Typecheck.
 
 Elpi Command declare_pred.
 Elpi Accumulate lp:{{
-pred mk-arg i:string, o:string.
-mk-arg Ty D :- D is "i:(" ^ Ty ^ ")".
-main [str P|Args] :-
-  std.map Args (x\r\x = str r) Args1,
-  D is "pred " ^ P ^ " " ^ {std.string.concat "," Args1} ^ ".",
-  % coq.say D,
-  coq.elpi.add-predicate "search.db" D.
+  pred mk-arg i:string, o:string.
+  mk-arg Ty D :- D is "i:(" ^ Ty ^ ")".
+  main [str P|Args] :-
+    std.map Args (x\r\x = str r) Args1,
+    D is "pred " ^ P ^ " " ^ {std.string.concat "," Args1} ^ ".",
+    coq.elpi.add-predicate "search.db" D.
 }}.
 Elpi Typecheck.
 
 Elpi Command accumulate_pred.
 Elpi Accumulate Db search.db.
 Elpi Accumulate lp:{{
-main [str P,int I] :-
-  coq.elpi.predicate P [I] Q,
-  coq.elpi.accumulate _ "search.db" (clause _ _ Q).
+  main [str P | L] :-
+    coq.elpi.predicate P {unwrap L} Q,
+    coq.elpi.accumulate _ "search.db" (clause _ _ Q).
 }}.
 Elpi Typecheck.
+
+Elpi declare_pred "s" "i:int, o:int".
+Elpi accumulate_pred "s" 0 3.
+Elpi accumulate_pred "s" 0 4.
+Elpi search "output" "s" 0.
 
 Elpi search "p" 1.
 Fail Elpi search "p" 2.
@@ -54,4 +63,10 @@ Fail Elpi search "q".
 
 Elpi declare_pred "r" "o:int".
 Elpi accumulate_pred "r" 1.
-Elpi search "r".
+Elpi search "output" "r".
+
+Elpi declare_pred "tt" "o:term, o:term".
+Elpi accumulate_pred "tt" (0) (1).
+Elpi search "output" "tt" (0).
+Fail Elpi search "output" "tt" (1).
+Elpi search "tt" (0) (1).
