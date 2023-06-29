31c527a9ef67e9fa6532436cee835e5a910b3f8a
Pierre Roux <pierre.roux@onera.fr>
2023-06-29 13:58:25 +0200

    Add example

--- apps/coercion/tests/test_coercion.v
+++ apps/coercion/tests/test_coercion.v
@@ -11,6 +11,28 @@ Elpi Typecheck coercion.
 Check true : Prop.
 Check false : Prop.
 
+Parameter ringType : Type.
+Parameter ringType_sort : ringType -> Type.
+Parameter natmul : forall (R : ringType) (n : nat), (ringType_sort R).
+
+Elpi Accumulate coercion.db lp:{{
+
+coercion _ N {{ nat }} {{ ringType_sort lp:R }} {{ natmul lp:R lp:N }} :-
+  coq.typecheck R {{ ringType }} ok.
+
+}}.
+Elpi Typecheck coercion.
+
+Section TestNatMul.
+
+Variable R : ringType.
+Variable n : nat.
+
+Check natmul R n : ringType_sort R.
+Check n : ringType_sort R.
+
+End TestNatMul.
+
 (* To inspect already declared coercions, do *)
 Elpi Print coercion "filename.html" "coercion".
 (* then open filename.html with a browser and
