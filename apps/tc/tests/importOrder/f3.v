6e14fdeb39b9f07cf802a4e99d5582cb015d0e35
Davide Fissore <fisso9900@gmail.com>
2023-07-06 17:19:01 +0200

    Test on instance order (To be finished)

--- apps/tc/tests/importOrder/f3.v
+++ apps/tc/tests/importOrder/f3.v
@@ -1,20 +1,23 @@
 From elpi.apps.tc.tests.importOrder Require Import f1.
 
-(* Module M1.
+Elpi Command SameOrderImport.
+Elpi Accumulate Db tc.db.
+Elpi Accumulate lp:{{
+  main _ :-
+    std.findall (instance _ _ _) RulesInst,
+    coq.TC.db DB_tc-inst,
+    std.map RulesInst (x\inst\ instance _Path inst _TC = x) Rules,
+    std.map DB_tc-inst (x\inst\ tc-instance inst _ = x) Rules2,
+    std.assert! (Rules = Rules2) "Error in import order [Elpi = Coq]".
+}}.
+
+Module M1.
   From elpi.apps.tc.tests.importOrder Require Import f2a.
   From elpi.apps.tc.tests.importOrder Require Import f2b.
 
-  Check (_ : A _).
-  (* Print HintDb typeclass_instances. *)
-
   Elpi Override TC TC_solver All.
 
-  Elpi Accumulate TC_solver lp:{{
-    :after "firstHook"
-    tc _ A _ :- coq.say "Searching for:" A, fail.
-  }}.
-
-  Check (_ : A _).
+  Elpi SameOrderImport.
 End M1.
 Reset M1.
 
@@ -22,62 +25,67 @@ Module M2.
   From elpi.apps.tc.tests.importOrder Require Import f2b.
   From elpi.apps.tc.tests.importOrder Require Import f2a.
 
-  Check (_ : A _).
-
   Elpi Override TC TC_solver All.
-
-  Elpi Accumulate TC_solver lp:{{
-    :after "firstHook"
-    tc _ A _ :- coq.say "Searching for:" A, fail.
-  }}.
-
-  Check (_ : A _).
+  
+  (* TODO: @gares @FissoreD why it fails *)
+  Fail Elpi SameOrderImport.
 End M2.
 Reset M2.
 
 Module M3.
-  Local Instance f3a : A nat := {f x := x}.
-  Local Instance f3b : A nat := {f x := x}.
-  Local Instance f3c : A nat := {f x := x}.
+  Global Instance f3a : A nat := {f x := x}.
+  Global Instance f3b : A nat := {f x := x}.
+  Global Instance f3c : A nat := {f x := x}.
+  Elpi AddAllInstances.
+  Elpi SameOrderImport.
 
   Section S1.
     Global Instance f3d : A nat := {f x := x}.
     Global Instance f3e : A nat := {f x := x}.
     Global Instance f3f : A nat := {f x := x}.
-    Print HintDb typeclass_instances.
-  End S1.
+    Elpi AddAllInstances.
+    Elpi SameOrderImport.
+  MySectionEnd.
+  Elpi SameOrderImport.
 
   Section S2.
     Context (T : Set).
     Global Instance f3g : A T := {f x := x}.
-  End S2.
+    Elpi AddAllInstances.
+    Elpi SameOrderImport.
+  MySectionEnd.
+  Elpi SameOrderImport.
 
   Section S3.
-    Global Instance f3h T1 T2 `(A T1, A T2) : A (T1 * T2) := {f x := x}.
-  End S3.
+    Context (T : Set).
+    Global Instance f3g2 : A (T: Set) := {f x := x}.
 
+    Global Instance f3h T1 T2 `(A T1, A T2) : A (T1 * T2) := {f x := x}.
+
+    Global Instance f3g3 : A (T: Set) := {f x := x}.
+    Global Instance f3g4 : A (T: Set) | 10 := {f x := x}.
+
+    Elpi AddAllInstances.
+    (* TODO: order should be corrected *)
+    Fail Elpi SameOrderImport.
+  MySectionEnd.
+
+  Fail Elpi SameOrderImport.
 End M3.
 
 Reset M3.
 
 Module M4.
   From elpi.apps.tc.tests.importOrder Require Import f2b.
-  Elpi AddAllInstances.
-
-  Print HintDb typeclass_instances.
-
-  Elpi Print TC_solver "TC_solver.html" ".*elpi.[^a].*" ".*:.*[0-9]+".
-
+  Elpi SameOrderImport.
 
   Global Instance f3a' T1 T2 `(A T1, A T2) : A (T1 * T2) := {f x := x}.
   Module M4'.
     From elpi.apps.tc.tests.importOrder Require Import f2a.
-    (* Elpi AddAllInstances. *)
-    Print HintDb typeclass_instances.
+    Fail Elpi SameOrderImport.
 
     Global Instance f3a : A nat := {f x := x}.
     Elpi AddInstances f3a.
-    Print HintDb typeclass_instances.
 
     Section S1.
       Global Instance f3b : A nat := {f x := x}.
@@ -85,22 +93,16 @@ Module M4.
       Section S1'.
         Global Instance f3c : A nat := {f x := x}.
         Elpi AddInstances f3c.
-        Print HintDb typeclass_instances.
       MySectionEnd.
-      Print HintDb typeclass_instances.
     MySectionEnd.
-    Print HintDb typeclass_instances.
+
+    Fail Elpi SameOrderImport.
 
     Section S2.
       Global Instance f3h T1 T2 `(A T1, A T2) : A (T1 * T2) := {f x := x}.
       Elpi AddInstances f3h.
-      Print HintDb typeclass_instances.
     MySectionEnd.
   End M4'.
-
-  Print HintDb typeclass_instances.
-  (* Elpi AddAllInstances. *)
-  Elpi Print TC_solver "TC_solver1.html" ".*elpi.[^a].*" ".*:.*[0-9]+". 
 End M4.
 
 Reset M4.
@@ -127,9 +129,7 @@ Module M5.
   End M4'.
 
   Elpi AddAllInstances. 
-  Elpi Print TC_solver "TC_solver1.html" ".*elpi.[^a].*" ".*:.*[0-9]+".
-  Print HintDb typeclass_instances.
-
+  Fail Elpi SameOrderImport.
 End M5.
 
 Reset M5.
@@ -138,19 +138,18 @@ Module M6.
   Global Instance f3a : A nat := {f x := x}.
   Global Instance f3b : A nat := {f x := x}.
   Global Instance f3c : A nat := {f x := x}.
-  (* Elpi AddAllInstances. *)
-  Elpi Print TC_solver "TC_solver.html" ".*elpi.[^a].*" ".*:.*[0-9]+".
-
+  
   Section S1.
     Global Instance f3d : A nat := {f x := x}.
     Global Instance f3e : A nat := {f x := x}.
     Global Instance f3f : A nat := {f x := x}.
   MySectionEnd.
   Elpi AddAllInstances.
-  Elpi Print TC_solver "TC_solver.html" ".*elpi.[^a].*" ".*:.*[0-9]+".
+
+  Elpi SameOrderImport.
 End M6.
 
-Reset M6. *)
+Reset M6.
 
 Module M7.
   Section S1.
@@ -161,24 +160,10 @@ Module M7.
       Context (T1 : Set).
       Global Instance f3b : A T1 := {f x := x}.
       Elpi AddInstances f3b.
-      Elpi print_instances.
-
-      (* Elpi Query TC_solver lp:{{
-        coq.env.section SVars1,
-        coq.env.end-section,
-        coq.env.section SVars2
-      }}. *)
-
     MySectionEnd.
-    Elpi print_instances.
+   Elpi SameOrderImport.
   MySectionEnd.
-  Elpi print_instances.
-  Print HintDb typeclass_instances.
-
-  (* TODO: instance f3b depends on T1 in S2, after exiting S2, f3b 
-  has no more section dependency *)
-
-  Elpi Print TC_solver "TC_solver.html" ".*elpi.[^a].*" ".*:.*[0-9]+".
+  Fail Elpi SameOrderImport.
 End M7.
 
 Reset M7.
@@ -190,11 +175,5 @@ Module M8.
   Global Instance I (a b c d: Type): Classe a a -> Classe b c. Admitted.
 
   Elpi AddAllInstances.
-  
-  Print HintDb typeclass_instances.
-
-  Elpi Query TC_solver lp:{{
-    get-inst-prio {{:gref I}} X, !,
-    std.assert! (X = 3) "CIAO".
-  }}.
+  Elpi SameOrderImport.
 End M8.
