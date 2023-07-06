8a2a8f658751bf2a3557e379ca0c6f2a71804959
Davide Fissore <fisso9900@gmail.com>
2023-07-06 16:23:20 +0200

    Remove old files

--- apps/tc/tests/old/eqDec/tc_lib_hardcoded_test.v
+++
@@ -1,7 +0,0 @@
-From elpi.apps Require Import tc.eqDec.tc_lib_hardcoded.
-
-Elpi Override TC hardcoded_solver.
-Check ((fun n m : nat  => n == m)).
-Compute (true == false). 
-Check (fun b q : bool => b == q).
-Check ((fun n m:prod nat bool => n == m)).
