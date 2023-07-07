ea1e8363917d5c90d51300ff7c626856ce355151
Davide Fissore <fisso9900@gmail.com>
2023-07-07 11:23:44 +0200

    add remove-eta good version

--- apps/tc/tests/removeEta.v
+++ apps/tc/tests/removeEta.v
@@ -1,7 +1,37 @@
 From elpi Require Import compiler.
 
 Elpi Query TC_solver lp:{{
-  remove-eta (fun _ _ (x\ app [{{3}}, x])) (app [{{3}}]),
-  remove-eta (fun _ _ (x\ app [{{3}}, x, x])) (fun _ _ (x\ app [{{3}}, x, x])),
-  remove-eta (fun _ _ (x\ {{3}})) (fun _ _ (x\ {{3}}))
+  remove-eta2 {{fun x => 3 x}} {{3}}
+}}.
+
+Elpi Query TC_solver lp:{{
+  remove-eta2 {{fun x => 3 x x}} {{fun x => 3 x x}}
+}}.
+
+Elpi Query TC_solver lp:{{
+  remove-eta2 {{fun x => 3}} {{fun x => 3}}
+}}.
+
+Elpi Query TC_solver lp:{{
+  remove-eta2 {{fun x => 3 (fun y => 4 y) x}} {{3 4}}
+}}.
+
+Elpi Query TC_solver lp:{{
+  remove-eta2 {{fun x => 3 (fun y => x y)}} {{3}}
+}}.
+
+Elpi Query TC_solver lp:{{
+  remove-eta2 {{fun x y => 3 x y}} {{3}}
+}}.
+
+Elpi Query TC_solver lp:{{
+  remove-eta2 {{fun x y => 3 y x}} {{fun x y => 3 y x}}
+}}.
+
+Elpi Query TC_solver lp:{{
+  remove-eta2 {{fun x y => 3 _ y}} {{fun x => 3 _}}
+}}.
+
+Elpi Query TC_solver lp:{{
+  remove-eta2 {{fun x y => 3 _ _}} {{fun x y => 3 _ _}}
 }}.
