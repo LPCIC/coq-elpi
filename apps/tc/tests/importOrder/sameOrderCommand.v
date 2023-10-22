825c084b4dd501913d212a6e0f3fae655ede478c
Davide Fissore <fisso9900@gmail.com>
2023-10-22 20:21:30 +0200

    Refactor instance/class observer

--- apps/tc/tests/importOrder/sameOrderCommand.v
+++ apps/tc/tests/importOrder/sameOrderCommand.v
@@ -1,14 +1,32 @@
 From elpi Require Export tc.
 
+From elpi.apps.tc Extra Dependency "base.elpi" as base.
+From elpi.apps.tc Extra Dependency "compiler.elpi" as compiler.
+From elpi.apps.tc Extra Dependency "tc_aux.elpi" as tc_aux.
+From elpi.apps.tc Extra Dependency "create_tc_predicate.elpi" as create_tc_predicate.
+
+Elpi Command add_instance.
+Elpi Accumulate File base.
+Elpi Accumulate File tc_aux.
+Elpi Accumulate Db tc.db.
+Elpi Accumulate File create_tc_predicate.
+Elpi Accumulate File compiler.
+Elpi Accumulate lp:{{
+  main [trm (global Inst), trm (global TC), str Locality, int Prio] :- 
+    add-inst Inst TC Locality Prio.
+
+  main [trm (global GR)] :- 
+    add-class-gr classic GR.
+}}.
+Elpi Typecheck.
+Elpi Override Register add_instance.
+Elpi Override TC TC_solver All.
+
+
+From elpi.apps.tc.tests.importOrder Extra Dependency "tc_same_order.elpi" as tc_same_order.
+
 Elpi Command SameOrderImport.
 Elpi Accumulate Db tc.db.
-Elpi Accumulate lp:{{
-  main _ :-
-    std.findall (instance _ _ _) RulesInst,
-    coq.TC.db DB_tc-inst,
-    std.map RulesInst (x\inst\ instance _Path inst _TC = x) RulesElpi,
-    std.map DB_tc-inst (x\inst\ tc-instance inst _ = x) RulesCoq,
-    if (RulesElpi = RulesCoq) true (
-      coq.error "Error in import order\n" 
-      "Expected :" RulesCoq "\nFound   :" RulesElpi).
-}}.
+Elpi Accumulate File base.
+Elpi Accumulate File tc_same_order.
+Elpi Typecheck.
