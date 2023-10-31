c928beeb5270e8cfca3e67ad1b3aa4165b7497b6
Davide Fissore <fisso9900@gmail.com>
2023-10-31 17:41:11 +0100

    coq option to activate compiler w/pattern fragment

--- apps/tc/theories/tc.v
+++ apps/tc/theories/tc.v
@@ -97,7 +97,8 @@ Elpi Accumulate File solver.
 Elpi Query lp:{{
   sigma Options\ 
     Options = [oTC-ignore-eta-reduction, oTC-resolution-time, 
-      oTC-clauseNameShortName, oTC-time-refine, oTC-debug, oTC-addModes],
+      oTC-clauseNameShortName, oTC-time-refine, oTC-debug, oTC-addModes,
+      oTC-use-pattern-fragment-compiler],
     std.forall Options (x\ sigma Args\ x Args, 
       coq.option.add Args (coq.option.bool ff) ff).
 }}.
