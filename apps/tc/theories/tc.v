908d7ce4ea988466e645fd5b1d7e72596229502b
Davide Fissore <fisso9900@gmail.com>
2023-10-31 17:51:58 +0100

    move addAllInstances-Classes ... in dedicated file

--- apps/tc/theories/tc.v
+++ apps/tc/theories/tc.v
@@ -12,6 +12,7 @@ From elpi.apps.tc Extra Dependency "tc_aux.elpi" as tc_aux.
 From elpi.apps.tc Extra Dependency "create_tc_predicate.elpi" as create_tc_predicate.
 
 From elpi.apps Require Import db.
+From elpi.apps Require Import add_commands.
 
 Elpi Command print_instances.
 Elpi Accumulate Db tc.db.
@@ -25,65 +26,6 @@ Elpi Accumulate lp:{{
     coq.say "Instances list is:" Rules.  
 }}.
 Elpi Typecheck. 
-
-Elpi Command AddAllInstances_.
-Elpi Accumulate Db tc.db.
-Elpi Accumulate Db tc_options.db.
-Elpi Accumulate File base.
-Elpi Accumulate File tc_aux.
-Elpi Accumulate File modes.
-Elpi Accumulate File compiler.
-Elpi Accumulate lp:{{  
-  main L :- 
-    args->str-list L L1,
-    std.forall {coq.TC.db-tc} (x\ add-tc-or-inst-gr [] L1 [x]).
-}}.
-Elpi Typecheck.
-
-Elpi Command AddInstances_.
-Elpi Accumulate Db tc.db.
-Elpi Accumulate Db tc_options.db.
-Elpi Accumulate File base.
-Elpi Accumulate File tc_aux.
-Elpi Accumulate File modes.
-Elpi Accumulate File compiler.
-Elpi Accumulate File parser_addInstances.
-Elpi Accumulate lp:{{
-  main Arguments :- 
-    parse Arguments Res, run-command Res.
-}}.
-Elpi Typecheck.
-
-Elpi Command AddHook.
-Elpi Accumulate Db tc.db.
-Elpi Accumulate Db tc_options.db.
-Elpi Accumulate File base.
-Elpi Accumulate File tc_aux.
-Elpi Accumulate lp:{{
-  pred addHook i:grafting, i:string. 
-  addHook Grafting NewName :- 
-    @global! => add-tc-db NewName Grafting (hook NewName).
-
-  main [str "before", str OldHook, str NewHook] :-
-    addHook (before OldHook) NewHook. 
-  
-  main [str "after", str OldHook, str NewHook] :-
-    addHook (after OldHook) NewHook. 
-
-  main [Graft, int OldHook, NewHook] :-
-     main [Graft, str {calc (int_to_string OldHook)}, NewHook]. 
-
-  main [Graft, OldHook, int NewHook] :-
-     main [Graft, OldHook, str {calc (int_to_string NewHook)}]. 
-
-  main _ :- 
-    coq.error "Invalid call to command AddHook. A valid call looks like"
-              "[ElpiAddHook Pos OldName NewName] where:"
-              " - Pos is either after or before"
-              " - OldName is the name of an existing hook"
-              " - NewName is the name of the new hook".
-}}.
-Elpi Typecheck.
 
 Elpi Tactic TC_solver.
 Elpi Accumulate Db tc.db.
@@ -112,37 +54,6 @@ Elpi Query lp:{{
       @global! => add-tc-db NumStr (before "lastHook") (hook NumStr)
     )
 }}.
-
-Elpi Command AddClasses_.
-Elpi Accumulate Db tc.db.
-Elpi Accumulate Db tc_options.db.
-Elpi Accumulate File base.
-Elpi Accumulate File tc_aux.
-Elpi Accumulate File create_tc_predicate.
-Elpi Accumulate lp:{{
-  main L :-
-    std.mem {attributes} (attribute "deterministic" _),
-    std.forall {args->str-list L} (add-class-str deterministic).
-  main L :- std.forall {args->str-list L} (add-class-str classic).
-  main _ :- coq.error "This commands accepts: [classic|deterministic]? TC-names*".
-}}.
-Elpi Typecheck.
-
-(* 
-  Adds all classes in the db. 
-*)
-Elpi Command AddAllClasses_.
-Elpi Accumulate Db tc.db.
-Elpi Accumulate Db tc_options.db.
-Elpi Accumulate File base.
-Elpi Accumulate File tc_aux.
-Elpi Accumulate File create_tc_predicate.
-Elpi Accumulate lp:{{
-  main _ :-
-    coq.TC.db-tc TC,
-    std.forall TC (add-class-gr classic).
-}}.
-Elpi Typecheck.
 
 Elpi AddAllClasses_.
 Elpi AddAllInstances_.
