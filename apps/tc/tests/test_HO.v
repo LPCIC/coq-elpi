baedca289a3c825f800e436477132c783731846a
Enrico Tassi <Enrico.Tassi@Inria.fr>
2024-01-09 14:49:03 +0100

    HO support

--- apps/tc/tests/test_HO.v
+++ apps/tc/tests/test_HO.v
@@ -6,13 +6,15 @@ Module FO_app.
 
 Class nice_predicate {T : Type} (P : T -> Prop).
 
-Lemma partial_app: forall (T : Type) (P : T -> T -> Prop), forall x, nice_predicate (P x). intros. split. Qed.
+Instance partial_app: forall (T : Type) (P : T -> T -> Prop), forall x, nice_predicate (P x). Qed.
+
+Elpi Print TC.Solver.
 
 Elpi Accumulate TC.Solver lp:{{
 
 % Since (P X) would be too HO for elpi (not pattern fragment), we use the FO approximation
-tc-nice_predicate T (app L) {{ @partial_app lp:T lp:P lp:X }} :-
-  unify-FO L 1 P [X].
+%tc-nice_predicate T (app L) {{ @partial_app lp:T lp:P lp:X }} :-
+%  unify-FO L 1 P [X].
 
 % Since (P x) has type (prod _ _ _) we also want to support eta for the class
 tc-nice_predicate T (fun _ _ _ as F) S :-
@@ -48,58 +50,6 @@ Lemma ex4 (T : Type) (p : nat -> T -> T -> Prop) y : nice_predicate (fun x => p 
   apply (@partial_app T (p 0) y).
 Abort.
 
-(*
-  Elpi Accumulate TC.AddInstances lp:{{
-    pred doo.
-    pred mem-arity o:term, o:term, o:int.
-
-    pred get_ty_arity i:term, i:term, i:term, o:prop.
-    get_ty_arity Ty Var New (mem-arity Var New N) :- coq.count-prods Ty N, not (N = 0).
-
-    :before "1"
-    compile-aux (prod N T F) I RevPremises ListVar IsPositive Clause :-
-      (if (IsPositive = tt) (Clause = pi x y\ C x y) (Clause = (pi x\ decl x N T => C x y))),
-      pi p zz\ sigma NewPremise TC L MemArity\
-        get_ty_arity T p zz MemArity, !,
-        MemArity => (
-          (doo => if (get-TC-of-inst-type T TC, coq.TC.class? TC /*, not (occurs p (F p))*/)
-          (compile-aux T p [] [] {neg IsPositive} NewPremise,
-            (L = [NewPremise | RevPremises])) (L = RevPremises)),
-        compile-aux (F p) I L [p | ListVar] IsPositive (C p zz)).
-    :before "1"
-    compile-aux (prod N T F) I RevPremises ListVar IsPositive Clause :- !,
-      (if (IsPositive = tt) (Clause = pi x\ C x) (Clause = (pi x\ decl x N T => C x))),
-      pi p\ sigma NewPremise TC L\
-        (if (get-TC-of-inst-type T TC, coq.TC.class? TC /*, not (occurs p (F p))*/)
-          (doo => compile-aux T p [] [] {neg IsPositive} NewPremise,
-            (L = [NewPremise | RevPremises])) (L = RevPremises),
-        compile-aux (F p) I L [p | ListVar] IsPositive (C p)).
-
-    pred copy-ty i:list prop, i:term, o:term, o:list prop.
-    copy-ty [] T T1 [] :- copy T T1.
-    copy-ty [(mem-arity OLD NEW ARITY)|TL] T T1 [split-app1 NEW OLD X|L] :-
-      % (pi A B\ copy (prod A B (x\ app [OLD, x])) (prod A B (x\ NEW x)) :- coq.error "STOP") =>
-      (copy (app [OLD|X]) NEW) => copy-ty TL T T1 L.
-
-    :before "1"
-    compile-aux Ty I RevPremises ListVar _ Clause :- !,
-      std.rev RevPremises Premises,
-      coq.mk-app I {std.rev ListVar} AppInst,
-      std.findall (mem-arity _ _ _) Mem,
-      if (not doo) (
-          std.findall (mem-arity _ _ _) L,
-          copy-ty L Ty Ty' Prem',
-          std.append Prem' Premises Premises'
-      ) (Ty = Ty', Premises' = Premises),
-      make-tc Ty' AppInst Premises' Clause.
-    :after "0"
-    add-tc-db _ _ C :- coq.say C, fail.
-  }}.
-  Elpi Query TC.Solver lp:{{
-    3 = 3.
-  }}.
-
-*)
 
 Elpi Override TC TC.Solver All.
 
@@ -111,23 +61,26 @@ Module HO_PF.
 
 Class Extensionality (T : Type).
 
-Lemma fun_1 (A1 : Type) (A2 : A1 -> Type) : Extensionality (forall a : A1, A2 a). split. Qed.
+Instance fun_1 (A1 : Type) (A2 : A1 -> Type) : Extensionality (forall a : A1, A2 a). Qed.
+
+
+Elpi Print TC.Solver.
 
 Elpi Accumulate TC.Solver lp:{{
 
 % Since app[A2, a] is in the pattern fragment we rephrase it
 % as (A2_PF a) and A2 = {{ fun x => lp:(A2_PF x) }} and then
 % eta contract
-tc-Extensionality (prod _ A1 a\ A2_PF a) {{ @fun_1 lp:A1 lp:A2 }} :-
-  coq.reduction.eta-contract {{ fun a : lp:A1 => lp:(A2_PF a) }} A2.
+%tc-Extensionality (prod _ A1 a\ A2_PF a) {{ @fun_1 lp:A1 lp:A2 }} :-
+%  coq.reduction.eta-contract {{ fun a : lp:A1 => lp:(A2_PF a) }} A2.
 
 % this simple version would work for odd, but not for the x = x + 1 example.
 % in the lucky case of odd, we would not need the eta contraction
-tc-Extensionality (prod _ A1 a\ app [A2, a]) {{ @fun_1 lp:A1 lp:A2 }}.
+% tc-Extensionality (prod _ A1 a\ app [A2, a]) {{ @fun_1 lp:A1 lp:A2 }}.
 % this is the version which is easy to explain but in the odd case
 % generates an ugly expansion
-tc-Extensionality (prod _ A1 a\ A2_PF a) {{ @fun_1 lp:A1 lp:A2 }} :-
-  A2 = {{ fun a : lp:A1 => lp:(A2_PF a) }}.
+% tc-Extensionality (prod _ A1 a\ A2_PF a) {{ @fun_1 lp:A1 lp:A2 }} :-
+%  A2 = {{ fun a : lp:A1 => lp:(A2_PF a) }}.
 
 }}.
 Elpi Typecheck TC.Solver.
