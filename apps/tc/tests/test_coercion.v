2e7bdd26a4e8bdfe7816bd136dc5d48159d6f19e
Davide Fissore <fisso9900@gmail.com>
2024-01-14 11:51:36 +0100

    Incorrectly compiled coercion instances are removed

--- apps/tc/tests/test_coercion.v
+++ apps/tc/tests/test_coercion.v
@@ -1,18 +1,46 @@
 From elpi Require Import tc.
 
-Set TC NameShortPath.
-
 Module Animals.
 
-  Class Animal.
+  Module Bird1.
+    Inductive info := Fly | NotFly.
 
-  Class Bird := IsAnimal :> Animal.
-  Instance dove : Bird. split. Qed.
+    Class Animal (i : info).
+    
+    Class Bird (i : info) := IsAnimal :> Animal i.
 
-  Elpi Query TC.Solver lp:{{
-    tc-Animal S, ground_term S.
-  }}.
+    Instance dove : Bird Fly. split. Qed.
 
-  Goal Animal. apply _. Qed.
+    (* It exists a ground solution for tc-Animal *)
+    Elpi Query TC.Solver lp:{{
+      tc-elpi.apps.tc.tests.test_coercion.Animals.Bird1.tc-Animal _ S, ground_term S.
+    }}.
+
+    (* It does not exist a solution for tc-Animal with a flexible solution *)
+    Elpi Query TC.Solver lp:{{
+      not (tc-elpi.apps.tc.tests.test_coercion.Animals.Bird1.tc-Animal _ S, not (ground_term S)).
+    }}.
+
+    Goal Animal Fly. apply _. Qed.
+    Goal Animal NotFly. Fail solve [apply _]. Abort.
+
+  End Bird1.
+
+  Module Bird2.
+
+    Class Animal.
+
+    Class Bird1 := IsAnimal : Animal.
+
+    Instance dove : Bird1. split. Qed.
+
+    (* It does not exists an instance for Animal1 *)
+    Elpi Query TC.Solver lp:{{
+      not (tc-elpi.apps.tc.tests.test_coercion.Animals.Bird2.tc-Animal _).
+    }}.
+
+    Goal Animal. Fail solve [apply _]. apply IsAnimal. Abort.
+
+  End Bird2.
 
 End Animals.
