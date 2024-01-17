e88a0ca7de6696f308b329eb8b253d5054acba10
Quentin Vermande <quentin.vermande@orange.fr>
2024-01-17 15:13:21 +0100

    canonical structures hook

---
+++ apps/cs/tests/test_cs.v
@@ -0,0 +1,37 @@
+From elpi.apps Require Import cs.
+From Coq Require Import Bool.
+
+Structure S : Type :=
+  { sort :> Type }.
+
+Elpi Accumulate cs.db lp:{{
+
+canonical-solution _ {{ sort lp:Sol }} {{ nat }} :-
+  Sol = {{ Build_S nat }}.
+
+}}.
+Elpi Typecheck canonical_solution.
+
+Check 1.
+Check eq_refl _ : (sort _) = nat.
+Definition nat1 := nat.
+Check 2.
+Check eq_refl _ : (sort _) = nat1.
+Definition sort1 := sort.
+Check 3.
+Check eq_refl _ : (sort1 _) = nat.
+Check 4.
+Check eq_refl _ : (sort1 _) = nat1.
+
+
+Elpi Accumulate cs.db lp:{{
+
+canonical-solution _ {{ sort lp:Sol }} {{ bool }} :-
+  % typing is wired in, do we want this?
+  std.spy(Sol = {{ Prop }}).
+
+}}.
+Elpi Typecheck canonical_solution.
+
+
+Check eq_refl _ : (sort _) = bool.
