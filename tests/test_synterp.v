013af6b9c5bd8bb3e8a3df06b6da5e1f238a83ab
Enrico Tassi <Enrico.Tassi@Inria.fr>
2024-01-29 17:50:55 +0100

    tentative fix

--- tests/test_synterp.v
+++ tests/test_synterp.v
@@ -166,7 +166,7 @@ Elpi Accumulate lp:{{
   main _ :-
     D = (record "foo" {{ Type }} "mkfoo" (field [] "f" {{ Type }} _\end-record)),
     coq.typecheck-indt-decl D ok,
-    coq.env.add-indt D I.
+    coq.env.add-indt D I, coq.env.begin-module "x" none, coq.env.end-module _.
 }}.
 #[synterp] Elpi Accumulate lp:{{
   main _ :- coq.env.begin-module "x" none, coq.env.end-module _.
