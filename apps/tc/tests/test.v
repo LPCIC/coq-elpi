c39493cc596327a755cacd93e4a5f27d8ee922fa
Davide Fissore <fisso9900@gmail.com>
2024-04-19 15:03:38 +0200

    ho unif ok for simple HO

--- apps/tc/tests/test.v
+++ apps/tc/tests/test.v
@@ -1,22 +1,35 @@
 From elpi Require Import tc.
 
-Class A (t : nat -> nat) (t' : Type).
-Class B (t : nat) (t' : Type).
+Module simpleHO.
+  Class A (t : nat -> nat) (t' : Type).
+  Class B (t : nat) (t' : Type).
 
-(* Elpi Trace Browser. *)
-Instance Innnn: forall b (c : nat -> nat),
-  (forall a, B (c a) b) -> A c b.
-Qed.
+  Instance Innnn: forall b (c : nat -> nat),
+    (forall a, B (c a) b) -> A c b.
+  Qed.
 
-Instance Immmm : B 3 bool.
-Qed.
-Elpi Print TC.Solver.
+  Instance Immmm : B 3 bool. Qed.
 
-Goal exists x, A x bool.
-eexists.
-  apply (Innnn bool (fun _ => 3) (fun x => Immmm)).
-Elpi Override TC TC.Solver None.
-Set Elpi Typeclasses Debug.
-Timeout 3 apply _.
-Show Proof.
-Qed.
+  Goal exists x, A x bool.
+  Proof. 
+    eexists.
+    apply _.
+  Qed.
+End simpleHO.
+
+(* Module HO_swap.
+  Axiom (f : Type -> Type -> Type).
+
+  Class c1 (T : (Type -> Type -> Type)).
+  Class c2 (T : (Type -> Type -> Type)).
+
+  Instance a1 : forall (F : Type -> Type -> Type), 
+    c2 (fun x y => F y x) -> c1 F. Qed.
+
+  Instance b1 : c2 f. Qed.
+
+  (* Here use of maybe-eta *)
+  Goal c1 (fun x y => f y x).
+    apply _.
+  Qed.
+End HO_swap. *)
