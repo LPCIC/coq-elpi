a806993ff0a2f33d85174df05c5342fe90f01477
Davide Fissore <fisso9900@gmail.com>
2024-04-26 11:07:40 +0200

    big refactor: move instance compilation from compiler1.elpi to ho_compile.

--- apps/tc/theories/add_commands.v
+++ apps/tc/theories/add_commands.v
@@ -6,6 +6,7 @@ From elpi.apps Require Import db.
 From elpi.apps.tc Extra Dependency "base.elpi" as base.
 From elpi.apps.tc Extra Dependency "tc_aux.elpi" as tc_aux.
 From elpi.apps.tc Extra Dependency "ho_precompile.elpi" as ho_precompile.
+From elpi.apps.tc Extra Dependency "ho_compile.elpi" as ho_compile.
 From elpi.apps.tc Extra Dependency "compiler1.elpi" as compiler1.
 (* From elpi.apps.tc Extra Dependency "compiler.elpi" as compiler. *)
 From elpi.apps.tc Extra Dependency "modes.elpi" as modes.
@@ -22,6 +23,7 @@ Elpi Accumulate Db tc_options.db.
 Elpi Accumulate File base.
 Elpi Accumulate File tc_aux.
 Elpi Accumulate File ho_precompile.
+Elpi Accumulate File ho_compile.
 Elpi Accumulate File ho_unif.
 Elpi Accumulate File compiler1.
 Elpi Accumulate File modes.
@@ -38,6 +40,7 @@ Elpi Accumulate Db tc_options.db.
 Elpi Accumulate File base.
 Elpi Accumulate File tc_aux.
 Elpi Accumulate File ho_precompile.
+Elpi Accumulate File ho_compile.
 Elpi Accumulate File ho_unif.
 Elpi Accumulate File compiler1.
 Elpi Accumulate File modes.
