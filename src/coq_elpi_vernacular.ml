6cd8c2bc1c0c80c15068b017335b42466e8f88a3
Enrico Tassi <Enrico.Tassi@Inria.fr>
2024-07-10 13:50:11 +0200

    fix compilation

--- src/coq_elpi_vernacular.ml
+++ src/coq_elpi_vernacular.ml
@@ -194,6 +194,12 @@ let get_and_compile ?even_if_empty name : (EC.program * bool) option =
     Coq_elpi_utils.elpitime (fun _ -> Pp.(str(Printf.sprintf "Elpi: get_and_compile %1.4f" (Unix.gettimeofday () -. t))));
     res
 
+[%%if coq = "8.19" || coq = "8.20"]
+let feedback_error loc ei = Feedback.(feedback (Message(Error,loc,CErrors.iprint ei)))
+[%%else]
+let feedback_error loc ei = Feedback.(feedback (Message(Error,loc,[],CErrors.iprint ei)))
+[%%endif]
+
 let run_static_check query =
   let checker = 
     let elpi = P.ensure_initialized () in
@@ -206,7 +212,7 @@ let run_static_check query =
       let loc = Loc.get_loc (snd ei) in
       (* The loc is not "Loc.mergeable" with the loc of the Elpi Typecheck
          command, hence we generate an error feedback on the right loc *)
-      Feedback.(feedback (Message(Error,loc,CErrors.iprint ei)));
+      feedback_error loc ei;
       Exninfo.iraise ei
     in
   ()
