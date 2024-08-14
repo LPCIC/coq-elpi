76de4615595118d67c5be1a9d18fac2463a73508
Pierre Roux <pierre.roux@onera.fr>
2024-08-14 11:55:27 +0200

    [CI] Cleanup no longer useful overlays

--- .nix/config.nix
+++ .nix/config.nix
@@ -1,11 +1,11 @@
 let common-bundles = {
   hierarchy-builder.override.version = "master";
-  hierarchy-builder-shim.job = false;
   mathcomp.override.version = "master";
   odd-order.override.version = "master";
   mathcomp-analysis.override.version = "master";
   mathcomp-finmap.override.version = "master";
   mathcomp-classical.override.version = "master";
+  mathcomp-bigenough.override.version = "master";
 
   mathcomp-single-planB-src.job = false;
   mathcomp-single-planB.job = false;
@@ -20,12 +20,22 @@ let common-bundles = {
   default-bundle = "coq-8.19";
   bundles = {
 
-    "coq-8.19".coqPackages = common-bundles // {
-      coq.override.version = "8.19";
+    "coq-8.19" = {
+      coqPackages = common-bundles // {
+        coq.override.version = "8.19";
+      };
+      ocamlPackages = {
+        elpi.override.version = "v1.19.5";
+      };
     };
 
-    "coq-master".coqPackages = common-bundles // {
-      coq.override.version = "master";
+    "coq-master" = {
+      coqPackages = common-bundles // {
+        coq.override.version = "master";
+      };
+      ocamlPackages = {
+        elpi.override.version = "v1.19.5";
+      };
     };
       
     "coq-master-min-elpi" = {
