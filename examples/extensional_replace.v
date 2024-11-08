54f095aeb2ff47bf69c343ed26d4942f70a2b476
Yves Bertot <Yves.Bertot@inria.fr>
2024-11-08 09:01:58 +0100

    This version contains a minimal working example of a tactic performing replacements under lambdas, justified by extensionality and ring.
     - The two input formulas refer to a bound variable, which appears in the
       first lambda expression found in a top-down left-to-right traversal of
       the goal syntax tree (the bound variable cannot be forgotten in the
       second formula).
     - replacements occur in the lambda expression as justified by the
       extensionality tactic (hence the dependence on the
       FunctionalExtensionality package).
     - The replacement can be justified using the ring command, once the
       extensionality tactic has been called.

--- examples/extensional_replace.v
+++ examples/extensional_replace.v
@@ -4,7 +4,7 @@ From elpi Require Import elpi.
 Lemma ring_example x : x + 1 = 1 + x.
 Proof. ring. Qed.
 
-Lemma fail_ring_under l : map (fun x => x + 1) l = map (fun x => 1 + x) l.
+Goal forall l, map (fun x => x + 1) l = map (fun x => 1 + x) l.
 Proof.
 Fail replace (x + 1) with (1 + x) by ring.
 replace (fun x => x + 1) with (fun x => 1 + x).
@@ -32,42 +32,55 @@ Elpi Accumulate lp:{{
 
 Elpi Typecheck.
 
-Lemma toto l : map (fun x => x + 1) l = map (fun x => 1 + x) l.
-elpi show (fun x => x + y).
-apply fail_ring_under.
-Qed.
+Ltac prove_by_extensionality_and_ring term1 term2 :=
+  replace term1 with term2;[ |
+    let Var_name := fresh "mame_for_bound_variable" in
+      extensionality Var_name; ring
+  ].
 
 Elpi Tactic replace.
 
 Elpi Accumulate lp:{{
-  pred process i:argument, i:argument, i:term.
+  pred process i:argument, i:argument, i:term, o:term, o:term.
 
 % The header says we want to replace a formula with one unknown
 % by a formula with also one unknown in the Goal.
-  process (open-trm 1 (fun Name _Ty F1))
-          (open-trm 1 (fun _Name1 _Ty1 F2)) G :-
-% We expect the unknown to be instantiated by a bound variable in
-% the goal, so we look for lambdas in the goal.
-    ((pi T A T\ fold-map T A T [T | A] :- T = (fun _ _ _)) =>
-      fold-map G [] _ TopLambdas),
-      MSG is "variable " ^ {coq.name->id Name} ^ " is unknown",!,
-% We take the first lambda, and perform the modification in it.
-      std.assert! (TopLambdas = [fun Name2 Ty2 H | _]) MSG,
-      (pi C \ (copy (F1 C) (F2 C) => copy (H C) (H' C))),
-      ((copy (fun Name2 Ty2 H) (fun Name2 Ty2 H')) =>
-       copy G G'),
-  %     coq.term->string (fun Name2 Ty2 H) Inp,
-  %     coq.say Inp,
-  %    coq.term->string  (fun Name2 Ty2 H') Res,
-      coq.term->string G' Res,
-      coq.say Res.
+  process (open-trm 1 (fun Name _Ty F1))    % to be replaced
+          (open-trm 1 (fun _Name1 _Ty1 F2)) % to be inserted in place
+          GoalConcl                         % term in which to replace
+          FirstLambda                       % Lambda to replace
+          (fun Name2 Ty2 H'):-              % lambda to insert in place
 
-solve (goal _Ctx _Trigger Type _ [Arg1, Arg2]) _ :-
-    process Arg1 Arg2 Type.
+% The 1 in first and second arguments expresses there is exactly one
+% unknown in these terms (expected to be the same unknown, not checked here
+% We expect the unknown to be instantiated by a bound variable in
+% the first lambda in the goal.  We perform the replacement in that lambda
+% without checking that there is progress (in this preliminary version).
+
+% We look for lambdas in the goal.  Behware that
+% TopLambdas contains the expressions in reverse order of their
+% discovery in a left-to-right traversal.
+%  std.do! [
+    ((pi T A T\ fold-map T A T [T | A] :- T = (fun _ _ _)) =>
+      fold-map GoalConcl [] _ TopLambdas),
+    MSG is "variable " ^ {coq.name->id Name} ^ " is unknown",
+% We take the first lambda
+    std.assert! ({std.rev TopLambdas} = [FirstLambda | _]) MSG,
+    FirstLambda = (fun Name2 Ty2 H),
+% We perform the required replacement in the body of the lambda
+    (pi C \ copy (F1 C) (F2 C) => copy (H C) (H' C))
+  %]
+  .
+
+solve (goal _ _ Type _ [Arg1, Arg2] as G) GL :-
+    process Arg1 Arg2 Type Term1 Term2,
+    std.assert! (coq.ltac.call "prove_by_extensionality_and_ring"
+        [trm Term1, trm Term2] G GL) "ltac call failed".
 }}.
 
 Elpi Typecheck.
 
 Goal forall l, map (fun x => (x + 1) + 2) l = map (fun x => (1 + x) + 2) l.
-Proof. intros l.
-elpi replace (1 + x) (x + 1).
+Proof.
+now intros l; elpi replace (x + 1) (1 + x).
+Qed.
