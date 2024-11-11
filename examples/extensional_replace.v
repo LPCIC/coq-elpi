980824891a9fd03f3d60588f087e1679001f4730
Yves Bertot (he) <yves.bertot@inria.fr>
2024-11-11 08:57:16 +0100

    fixes identify proofs in applications (needed a sigma), plus csmetics the treatment of polymorphic is still wrong (it cannot use f_equal4 for map)

--- examples/extensional_replace.v
+++ examples/extensional_replace.v
@@ -52,6 +52,7 @@ Qed.
 About f_equal.
 About f_equal2.
 About f_equal3.
+Fail Check f_equal6.
 
 About eq_trans.
 About id.
@@ -71,13 +72,20 @@ Elpi Accumulate lp:{{
 
 pred mk-app-prf i:list term, i:list term, i: list term, o:term.
 
-mk-app-prf [F, _] [_, _] [{{@refl_equal _ _}}, P] {{f_equal lp:F lp:P}}.
-mk-app-prf [F, _, _] [_, _, _] [{{@refl_equal _ _}}, P1, P2]
+mk-app-prf [F, _] [F, _] [{{@refl_equal _ _}}, P] {{f_equal lp:F lp:P}}.
+mk-app-prf [F, _, _] [F, _, _] [{{@refl_equal _ _}}, P1, P2]
   {{f_equal2 lp:F lp:P1 lp:P2}}.
 mk-app-prf [F, _, _, _] [F, _, _, _] [{{@refl_equal _ _}}, P1, P2, P3]
   {{f_equal3 lp:F lp:P1 lp:P2 lp:P3}}.
+mk-app-prf [F, _, _, _, _] [F, _, _, _, _]
+  [{{@refl_equal _ _}}, P1, P2, P3, P4]
+  {{f_equal4 lp:F lp:P1 lp:P2 lp:P3 lp:P4}}.
+% use nary_congruence from ssreflect
+mk-app-prf [F, _, _, _, _, _] [F, _, _, _, _, _]
+  [{{@refl_equal _ _}}, P1, P2, P3, P4, P5]
+  {{f_equal5 lp:F lp:P1 lp:P2 lp:P3 lp:P4 lp:P5}}.
 
-pred fold-map2 i:list term i:A i:(term -> A -> term -> term -> A -> prop) 
+pred fold-map2 i:list term i:A i:(term -> A -> term -> term -> A -> prop)
   o:list term o:list term o:A.
 
 fold-map2 [] A _ [] [] A.
@@ -93,13 +101,13 @@ pred remove_one_unknown i:name, i:term, i:term, i:term, o:term.
 
 remove_one_unknown N _T C (fun N1 _T1 F) Res :-
   {coq.name->id N} = {coq.name->id N1},!,
- 
+
   Res = (F C),!,
    coq.say "remove the unknown" Res.
 
 remove_one_unknown N T C (fun N1 T1 F) (fun N1 T1 Res) :-
   coq.say "not the unknown" N N1,
-  (@pi-decl N1 T1 x \ 
+  (@pi-decl N1 T1 x \
      remove_one_unknown N T C (F x) (Res x)),!.
 
 instantiate N T C (open-trm 1 F) (trm Res) :-
@@ -112,7 +120,7 @@ instantiate N T C (open-trm I F) (open-trm J Res) :-
 instantiate _N _T _C (trm A) (trm A):- !.
 
 instantiate_pair N T C (pr A1 A2) (pr B1 B2) :-
-  std.do! [ coq.say "before" N T C A1 B1, 
+  std.do! [ coq.say "before" N T C A1 B1,
   std.assert! (instantiate N T C A1 B1) "first instantiate failed",
    coq.say "between" B1, instantiate N T C A2 B2,
    coq.say "after" N T C (pr B1 B2)].
@@ -124,89 +132,89 @@ mk-equality (pr (trm S) (trm T)) S A T P A :- !,
   coq.say "rewrite happens".
 
 :name "mk-equality:start"
-mk-equality _Ctx X A Z Y A :- name X,!, 
-% coq.say "first clause", 
+mk-equality _RW X A Z Y A :- name X,!,
+% coq.say "first clause",
 X = Z, {{@refl_equal _ lp:X}} = Y, !.
-mk-equality _Ctx (global _ as C) A C {{@refl_equal _ lp:C}} A :- !
+mk-equality _RW (global _ as C) A C {{@refl_equal _ lp:C}} A :- !
 % , coq.say "second clause" C
 .
-mk-equality _Ctx (pglobal _ _ as C) A C {{@refl_equal _ lp:C}} A :- !
+mk-equality _RW (pglobal _ _ as C) A C {{@refl_equal _ lp:C}} A :- !
 % , coq.say "third clause"
 .
-mk-equality _Ctx (sort _ as C) A C {{@refl_equal _ lp:C}} A :- !
+mk-equality _RW (sort _ as C) A C {{@refl_equal _ lp:C}} A :- !
 % , coq.say "fourth clause"
 .
 
-mk-equality Ctx (fun N T F as C) A C Res A :-
+mk-equality RW (fun N T F as C) A C Res A :-
 @pi-decl N T x\
-  (instantiate_pair N T x Ctx (Ctx1 x),
-   mk-equality (Ctx1 x) (F x) A _ {{@refl_equal _ _}} _A2,!,
+  (instantiate_pair N T x RW (RW1 x),
+   mk-equality (RW1 x) (F x) A _ {{@refl_equal _ _}} _A2,!,
    Res = {{@refl_equal _ _}},
    coq.say "fifth clause").
 
-mk-equality Ctx (fun N T F) A (fun N T F1) 
+mk-equality RW (fun N T F) A (fun N T F1)
   {{functional_extensionality lp:{{(fun N T F)}} lp:{{(fun N T F1)}}
       lp:{{(fun N T Prf)}}}} A1 :- !,
       coq.say "sixth clause",
 @pi-decl N T x\
-  (std.assert! (instantiate_pair N T x Ctx (Ctx1 x)) "instantiate_pair failed",!,
-   mk-equality (Ctx1 x) (F x) A (F1 x) (Prf x) A1).
+  (std.assert! (instantiate_pair N T x RW (RW1 x)) "instantiate_pair failed",!,
+   mk-equality (RW1 x) (F x) A (F1 x) (Prf x) A1).
 
 % TODO: write unit tests for the following clauses
-mk-equality Ctx (let N T B F as C) A C {{@refl_equal _ lp:C}} A :-
-mk-equality Ctx B A _ {{@refl_equal _ _}} A2,
+mk-equality RW (let N T B F as C) A C {{@refl_equal _ lp:C}} A :-
+mk-equality RW B A _ {{@refl_equal _ _}} A2,
 (@pi-decl N T x\
-  (std.assert! (instantiate_pair N T x Ctx (Ctx1 x)) "instantiate_pair failed",!,
-  mk-equality (Ctx1 x) (F x) A2 _ {{@refl_equal _ _}} _A3)),!,
+  (std.assert! (instantiate_pair N T x RW (RW1 x)) "instantiate_pair failed",!,
+  mk-equality (RW1 x) (F x) A2 _ {{@refl_equal _ _}} _A3)),!,
   coq.say "seventh clause".
 
-mk-equality Ctx (let N T B F) A (let N T B1 F1) 
+mk-equality RW (let N T B F) A (let N T B1 F1)
   {{let_congr lp:B lp:B1 lp:{{fun N T F}} lp:{{fun N T F1}}
       lp:PB lp:PF}} A3:- !,
-mk-equality Ctx B A B1 PB A2,
-@pi-decl N T x\ 
-  (instantiate_pair N T x Ctx (Ctx1 x),!,
+mk-equality RW B A B1 PB A2,
+@pi-decl N T x\
+  (instantiate_pair N T x RW (RW1 x),!,
   coq.say "eigth clause",
-   mk-equality (Ctx1 x) (F x) A2 (F1 x) PF A3).
+   mk-equality (RW1 x) (F x) A2 (F1 x) PF A3).
 
-mk-equality Ctx (prod N T F) A (prod N T F1) (fun N T P1) A2 :- !,
-(@pi-decl N T x\ 
-  (instantiate_pair N T x Ctx (Ctx1 x),!,
-   mk-equality  (Ctx1 x) (F x) A (F1 x) (P1 x) A2)),!
+mk-equality RW (prod N T F) A (prod N T F1) (fun N T P1) A2 :- !,
+(@pi-decl N T x\
+  (instantiate_pair N T x RW (RW1 x),!,
+   mk-equality  (RW1 x) (F x) A (F1 x) (P1 x) A2)),!
    , coq.say "ninth clause"
 .
-mk-equality Ctx (app L as C) A C {{@refl_equal _ lp:C}} A :-
-fold-map2 L A (mk-equality Ctx) _ L1 _A1,
-std.forall L1 (c \ c = {{@refl_equal _ _}}),!
+mk-equality RW (app L as C) A C {{@refl_equal _ lp:C}} A :-
+fold-map2 L A (mk-equality RW) _ L1 _A1,
+std.forall L1 (c \ (sigma A B \ c = {{@refl_equal lp:A lp:B}})),!
 , coq.say "tenth clause"
 .
 
-mk-equality Ctx {{@map lp:T1 lp:T2 lp:{{fun N _ F}} lp:L}}
-  A 
+mk-equality RW {{@map lp:T1 lp:T2 lp:{{fun N _ F}} lp:L}}
+  A
   {{@map lp:T1 lp:T2 lp:{{fun N T1 F1}} lp:L1}}
   R A2 :- !,
   % coq.say "eleventh clause",
   @pi-decl N T1 x\
-  (instantiate_pair N T1 x Ctx (Ctx1 x),!,
+  (instantiate_pair N T1 x RW (RW1 x),!,
     @pi-decl `H` (Ext_Hyp x) h\
-      mk-equality (Ctx1 x) (F x) A (F1 x) (Pf x h) A1),
-  mk-equality Ctx L A1 L1 Pl A2,
+      mk-equality (RW1 x) (F x) A (F1 x) (Pf x h) A1),
+  mk-equality RW L A1 L1 Pl A2,
   R = {{map_ext_in_equal2 lp:{{fun N _ F}} lp:{{fun N _ F1}} lp:L lp:L1
          lp:{{fun N T1 x\
               (fun `H` (Ext_Hyp x) h\ Pf x h)}} lp:Pl }}.
-   
-mk-equality Ctx (app L) A (app L1) Prf A1 :-
+
+mk-equality RW (app L) A (app L1) Prf A1 :-
 L = [Hd | _], coq.term->string Hd S,
 coq.say "entering twelfth" Hd S,
-fold-map2 L A (mk-equality Ctx) L1 P1 A1,
+fold-map2 L A (mk-equality RW) L1 P1 A1,
 mk-app-prf L L1 P1 Prf.
 
-mk-equality _Ctx (fix _N _Rno _Ty _F as C) A C {{@refl_equal _ lp:C}} A :- !.
-mk-equality _Ctx (match _T _Rty _B as C) A C {{@refl_equal _ lp:C}} A :- !.
-mk-equality _Ctx (primitive _ as C) A C {{@refl_equal _ lp:C}} A :- !.
-mk-equality _Ctx (uvar _M _L as C) A C {{@refl_equal _ lp:C}} A :- !.
+mk-equality _RW (fix _N _Rno _Ty _F as C) A C {{@refl_equal _ lp:C}} A :- !.
+mk-equality _RW (match _T _Rty _B as C) A C {{@refl_equal _ lp:C}} A :- !.
+mk-equality _RW (primitive _ as C) A C {{@refl_equal _ lp:C}} A :- !.
+mk-equality _RW (uvar _M _L as C) A C {{@refl_equal _ lp:C}} A :- !.
 % when used in CHR rules
-mk-equality _Ctx (uvar _X _L as C) A C {{@refl_equal _ lp:C}} A :- !.
+mk-equality _RW (uvar _X _L as C) A C {{@refl_equal _ lp:C}} A :- !.
 
 pred process i:argument, i:argument, i:term, o:term, o:term.
 
@@ -251,7 +259,7 @@ solve (goal _ _ Type _ [Arg1, Arg2] as G) GL :-
     std.assert! (coq.ltac.call "prove_by_extensionality_and_ring"
         [trm Term1, trm Term2] G GL) "ltac call failed".
 
-solve (goal _ _ {{ _ = lp:Y }} _ [_, Arg1, Arg2] as G) GL :-
+solve (goal _ _ {{lp:_X = lp:Y }} _ [_, Arg1, Arg2] as G) GL :-
   coq.say "calling equality with " (pr Arg1 Arg2) Y,
   mk-equality (pr Arg1 Arg2) Y [] Y2 P _,
   coq.say "mk-equality succeeded" P,!,
@@ -260,7 +268,7 @@ solve (goal _ _ {{ _ = lp:Y }} _ [_, Arg1, Arg2] as G) GL :-
   coq.say "prouf term" SP,
   std.assert-ok! (coq.typecheck P {{lp:Y = lp:Y2}}) "proof incorrect",
   coq.say "typecheck succeeded" S,!,
-  refine {{eq_sym (@eq_trans _ _ lp:Y2 _ lp:P (eq_sym _))}} G GL.
+  refine {{@eq_trans _ _ lp:Y2 _ _ (eq_sym lp:P)}} G GL.
   % refine {{eq_sym (@eq_trans lp:Y lp:Y2 lp:X lp:P (eq_sym _))}} G GL.
 
 solve (goal _ _ _ _ [Arg1, Arg2]) _ :-
@@ -330,7 +338,7 @@ Qed.
 (* The first test illustrates the case where is no unknown. *)
 Goal forall x, x = 1 -> 2 = x + 1.
 intros x x1.
-elpi replace True (x) (1);[assumption | ].
+elpi replace True (x) (1);[ | assumption].
 ring.
 Qed.
 
@@ -340,18 +348,20 @@ Goal forall l, map (fun x => x + 1) l = map (fun x => x + (1 + 0)) l.
 Proof.
 intros l.
 elpi replace True (x + (1 + 0)) (x + 1).
-  (* TODO: find how to have this goal beta-reduced. *)
-  lazy beta.
-  ring.
-easy.
+  easy.
+(* TODO: find how to have this goal beta-reduced. *)
+lazy beta.
+ring.
 Qed.
 
-(* We can now try a replacement in nested sums. *)
-Goal forall l, map (fun i => map (fun j => i + j) l) l =
-  map (fun i => map (fun j => j + i) l) l.
+Goal forall n, map (fun i => map (fun j => i + j) (map Z.of_nat (seq 1 n)))
+  (map Z.of_nat (seq 1 n)) =
+  map (fun i => map (fun j => j + i) (map Z.of_nat (seq 1 n)))
+    (map Z.of_nat (seq 1 n)).
 intros n.
 elpi replace True (j + i) (i + j).
-  lazy beta.
+  easy.
+(* TODO: find how to have this goal beta-reduced. *)
+lazy beta.
 ring.
-easy.
 Qed.
