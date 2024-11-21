0e4eb52e6a5babe391ec456f3cef8952d0efce78
Enrico Tassi <Enrico.Tassi@Inria.fr>
2024-11-21 15:29:41 +0100

    fix opam file

---
+++ tests/test_File2.v
@@ -0,0 +1,11 @@
+From elpi.tests Require Import test_File1.
+
+Elpi File file2 lp:{{ main _ :- coq.say "hello3". }}.
+
+Elpi Command c.
+Elpi Accumulate lp:{{ main _ :- coq.say "hello1". }}.
+Elpi Accumulate File file1.
+Elpi Accumulate File file2.
+Elpi Accumulate lp:{{ main _ :- coq.say "hello4". }}.
+
+Elpi c.
