3b861388a4eccc6d95c73b0e5140e13c59337de2
Pierre Roux <pierre.roux@onera.fr>
2024-12-16 11:45:57 +0100

    [CI] Update Nix toolbox
    Following the merge of
    https://github.com/coq-community/coq-nix-toolbox/pull/302

--- default.nix
+++ default.nix
@@ -4,8 +4,8 @@
   bundle ? null, job ? null, inNixShell ? null, src ? ./.,
 }@args:
 let auto = fetchGit {
-  url = "https://github.com/proux01/coq-nix-toolbox.git";
-  ref = "elpi206";
+  url = "https://github.com/coq-community/coq-nix-toolbox.git";
+  ref = "master";
   rev = import .nix/coq-nix-toolbox.nix;
 };
 in
