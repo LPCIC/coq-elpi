176089a5bbaf8d472aa8685f5fa431e90fc7f777
Patrick Nicodemus <gadget142@gmail.com>
2025-01-19 13:13:45 -0500

    Removed unnecessary code.

--- apps/eltac/theories/apply.v
+++ apps/eltac/theories/apply.v
@@ -7,7 +7,6 @@ Elpi Accumulate lp:{{
     apply T _ G GL :- refine T G GL.
 
     apply Term (prod _ A B) G GL :-
-        Hole = {{ _ }},
         coq.typecheck Hole A ok,
         whd (app [Term, Hole]) [] HD ARGS,
         unwind HD ARGS Term',
@@ -16,9 +15,7 @@ Elpi Accumulate lp:{{
     apply _ _ _ _ :- coq.ltac.fail _ "Couldn't unify type of term with goal".
         
     solve (goal Ctx _ _ _ [trm T] as G) GL :-
-        (% T is a direct reference to a global definition or axiom
-        T = global Gref, coq.env.typeof Gref Ty;
-        % T is a direct Gallina term and we will infer its type
+        (% T is a direct Gallina term and we will infer its type
         % from context
         coq.typecheck T Ty ok;
         % Eq is a reference to a declared variable in the context
