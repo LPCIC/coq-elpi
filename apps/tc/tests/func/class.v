a5ccc0ac3f98d4b892886f66d20c0938325ac988
Davide Fissore <fisso9900@gmail.com>
2025-05-20 14:44:57 +0200

    wip

--- apps/tc/tests/func/class.v
+++ apps/tc/tests/func/class.v
@@ -5,6 +5,5 @@ Elpi TC.deterministic.
 Class C (T : nat).
 
 Instance I1 : C 1. Qed.
-Instance I2 : C 1. Qed.
 
-Elpi Print TC.Solver "elpi.apps.tc.examples/TC.Solver".
+Fail Instance I2 : C 1. Qed.
