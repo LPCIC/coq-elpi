df76dea0e8c6dabbf885d1e08887b470dc357c9b
Davide Fissore <fisso9900@gmail.com>
2025-05-21 16:00:21 +0200

    wip: complete tests in func

--- apps/tc/tests/func/class.v
+++ apps/tc/tests/func/class.v
@@ -1,9 +1,10 @@
 From elpi.apps Require Import tc.
 
 Elpi TC.deterministic.
+Elpi TC.Pending_mode "+".
 
 Class C (T : nat).
 
 Instance I1 : C 1. Qed.
 
-Instance I2 : C 1. Fail Qed.
+Instance I2 : C 1. Fail Qed. Abort.
---
+++ apps/tc/tests/func/overlap.v
@@ -0,0 +1,4 @@
+From elpi.apps.tc.tests.func Require Import inst1.
+From elpi.apps.tc.tests.func Require Import inst2.
+
+(* Elpi Print TC.Solver "elpi.apps.tc.examples/TC.Solver.txt". *)
