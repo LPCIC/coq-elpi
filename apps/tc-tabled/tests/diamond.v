83f6e4a97e02a4287527b4126daae3927623ff80
Lasse Letager Hansen <letager@cs.au.dk>
2025-08-13 22:48:45 +0200

    Add update to map and do some testing for tower of diamonds

---
+++ apps/tc-tabled/tests/diamond.v
@@ -0,0 +1,117 @@
+(* https://github.com/leanprover/lean4/blob/master/tests/lean/run/typeclass_diamond.lean *)
+
+(* Diamond *)
+Class T (alpha : Type) (n : nat).
+Class R (alpha : Type) (n : nat).
+Class L (alpha : Type) (n : nat).
+Class B (alpha : Type) (n : nat).
+Instance BtL alpha n `{B alpha n} : L alpha n := {}.
+Instance BtR alpha n `{B alpha n} : R alpha n := {}.
+Instance LtR alpha n `{L alpha n} : T alpha n := {}.
+Instance RtR alpha n `{R alpha n} : T alpha n := {}.
+Instance TtR alpha n `{T alpha n} : B alpha (S n) := {}.
+
+Instance B0 alpha : B alpha 0 := {}.
+
+(* (* Finished transaction in 0. secs (0.u,0.s) (successful) *) *)
+(* Module Test0. Time Instance B_0 : B unit 0 := _. End Test0. *)
+
+(* (* Finished transaction in 1.389 secs (1.387u,0.s) (successful) *) *)
+(* Module Test225. Time Instance TtR225 : B unit 225 := _. End Test225. *)
+
+(* (* Finished transaction in 12.928 secs (12.913u,0.011s) (successful) *) *)
+(* Module Test450. Time Instance TtR450 : B unit 450 := _. End Test450. *)
+
+(* (* Finished transaction in 38.213 secs (38.175u,0.001s) (successful) *) *)
+(* Module Test675. Time Instance TtR675 : B unit 675 := _. End Test675. *)
+
+(* (* Finished transaction in 104.095 secs (103.999u,0.074s) (successful) *) *)
+(* Module Test900. Time Instance TtR900 : B unit 900 := _. End Test900. *)
+
+(* (* Finished transaction in 175.868 secs (175.7u,0.026s) (successful) *) *)
+(* Module Test1125. Time Instance TtR1125 : B unit 1125 := _. End Test1125. *)
+
+(* (* Finished transaction in 355.404 secs (355.286u,0.021s) (successful) *) *)
+(* Module Test1350. Time Instance B1350 : B unit 1350 := _. End Test1350. *)
+
+(* (* Finished transaction in 534.246 secs (533.92u,0.079s) (successful) *) *)
+(* Module Test1575. Time Instance TtR1575 : B unit 1575 := _. End Test1575. *)
+
+(* (* Finished transaction in 849.83 secs (849.087u,0.431s) (successful) *) *)
+(* Module Test1800. Time Instance B1800 : B unit 1800 := _. End Test1800. *)
+
+(* (* Finished transaction in 1098.921 secs (1098.166u,0.083s) (successful) *) *)
+(* Module Test2025. Time Instance TtR2025 : B unit 2025 := _. End Test2025. *)
+
+(* (* Finished transaction in 1441.027 secs (1439.305u,0.752s) (successful) *) *)
+(* Module Test2250. Time Instance B2250 : B unit 2250 := _. End Test2250. *)
+
+From elpi Require Import elpi.
+From elpi.apps Require Import tc.
+
+(* Elpi TC Solver Activate TC.Solver. *)
+Elpi TC Solver Override TC.Solver All.
+
+TC.AddAllClasses.
+TC.AddAllInstances.
+
+(* (* Finished transaction in 3.082 secs (3.052u,0.026s) (successful) *) *)
+(* (* Finished transaction in 5.362 secs (3.159u,0.319s) (successful) *) *)
+(* (* Finished transaction in 7.973 secs (3.183u,0.686s) (successful) *) *)
+(* Module Test700TC. Time Instance B700 : B unit 700 := _. End Test700TC. *)
+
+(* (* Finished transaction in 13.83 secs (13.794u,0.003s) (successful) *) *)
+(* (* Finished transaction in 12.364 secs (12.353u,0.s) (successful) *) *)
+(* (* Finished transaction in 12.292 secs (12.195u,0.019s) (successful) *) *)
+(* Module Test1400TC. Time Instance B1400 : B unit 1400 := _. End Test1400TC. *)
+
+(* (* Finished transaction in 29.792 secs (28.346u,0.261s) (successful) *) *)
+(* (* Finished transaction in 29.792 secs (28.346u,0.261s) (successful) *) *)
+(* Module Test2100TC. Time Instance B2100 : B unit 2100 := _. End Test2100TC. *)
+
+(* (* Finished transaction in 56.321 secs (54.822u,1.44s) (successful) *) *)
+(* Module Test2800TC. Time Instance B2800 : B unit 2800 := _. End Test2800TC. *)
+
+(* (* Finished transaction in 84.556 secs (82.008u,2.295s) (successful) *) *)
+(* Module Test3500TC. Time Instance B3500 : B unit 3500 := _. End Test3500TC. *)
+
+(*  *)
+Module Test4200TC. Time Instance B4200 : B unit 4200 := _. End Test4200TC.
+
+Elpi TC Solver Override TC.Solver None.
+
+(* From elpi.apps.tc_tabled Require Import tabled_typelass. *)
+
+(* (* Diamond example in Rocq *) *)
+(* Elpi TC Solver Activate TC.TabledSolver. *)
+(* Elpi TC Solver Override TC.TabledSolver All. *)
+
+(* (* Finished transaction in 1.911 secs (1.901u,0.007s) (successful) *) *)
+(* Module Test50Tabled. Time Instance B50 : B unit 50 := _. End Test50Tabled. (* Takes: 20000 - 19650 steps *) *)
+
+(* (* Finished transaction in 12.3 secs (12.253u,0.032s) (successful) *) *)
+(* Module Test100Tabled. Time Instance B100 : B unit 100 := _. End Test100Tabled. (* Takes: 20000 - 19300 steps *) *)
+
+(* (* Finished transaction in 50.408 secs (50.147u,0.152s) (successful) *) *)
+(* Module Test150Tabled. Time Instance B150 : B unit 150 := _. End Test150Tabled. (* Takes: 20000 - 18950 steps *) *)
+
+(* (* Finished transaction in 154.541 secs (153.917u,0.547s) (successful) *) *)
+(* Module Test200Tabled. Time Instance B200 : B unit 200 := _. End Test200Tabled. (* Takes: 20000 - 18600 steps *) *)
+
+(* (* Finished transaction in 435.689 secs (434.456u,0.983s) (successful) *) *)
+(* Module Test250Tabled. Time Instance B250 : B unit 250 := _. End Test250Tabled. (* Takes: 20000 - 18250 steps *) *)
+
+(* (* Finished transaction in 694.726 secs (693.17u,1.298s) (successful) *) *)
+(* Module Test300Tabled. Time Instance B300 : B unit 300 := _. End Test300Tabled. (* Takes: 20000 - 17900 steps *) *)
+
+(* (* Finished transaction in 1473.093 secs (1437.954u,6.426s) (successful) *) *)
+(* Module Test350Tabled. Time Instance B350 : B unit 350 := _. End Test350Tabled. (* Takes: 20000 - 17550 steps *) *)
+
+(* (* Finished transaction in 2457.3 secs (2447.5u,3.536s) (successful) *) *)
+(* Module Test400Tabled. Time Instance B400 : B unit 400 := _. End Test400Tabled. (* Takes: 20000 - 17200 steps *) *)
+
+(* (* Finished transaction in 3465.9 secs (3306.596u,16.99s) (successful) *) *)
+(* Module Test450Tabled. Time Instance B450 : B unit 450 := _. End Test450Tabled. (* Takes: 20000 - 16850 steps *) *)
+
+(* (*  *) *)
+(* Module Test500Tabled. Time Instance B500 : B unit 500 := _. End Test500Tabled. (* Takes: 20000 - 16500 steps *) *)
--- apps/tc-tabled/theories/tabled_type_class.v
+++ apps/tc-tabled/theories/tabled_type_class.v
@@ -9,9 +9,11 @@ Declare ML Module "rocq-elpi.tc-tabled".
 From elpi.apps.tc_tabled.elpi Extra Dependency "custom_map.elpi" as custom_map.
 From elpi.apps.tc_tabled.elpi Extra Dependency "map2.elpi" as map2.
 From elpi.apps.tc_tabled.elpi Extra Dependency "tabled_type_class.elpi" as TabledTC.
+From elpi.apps.tc_tabled.elpi Extra Dependency "avl.elpi" as avl.
 
 Elpi Accumulate File custom_map.
 Elpi Accumulate File map2.
+Elpi Accumulate File avl.
 
 Elpi Accumulate lp:{{
   pred mymap_make i:(pred i:K, i:K, o:cmp), o:mymap K V.
@@ -19,12 +21,14 @@ Elpi Accumulate lp:{{
   pred mymap_add i:K i:V i:(mymap K V) o:(mymap K V).
   pred mymap_update i:K i:V i:(mymap K V) o:(mymap K V).
 
-  %% typeabbrev (mymap K V) (custom_map K V).
+  /*
+  typeabbrev (mymap K V) (custom_map K V).
 
-  %% mymap_make Cmp M :- custom_make Cmp M.
-  %% mymap_find K M V :- custom_find K M V.
-  %% mymap_add K V M M1 :- custom_add K V M M1.
-  %% mymap_update K V M M1 :- custom_add K V M M1.
+  mymap_make Cmp M :- custom_make Cmp M.
+  mymap_find K M V :- custom_find K M V.
+  mymap_add K V M M1 :- custom_add K V M M1.
+  mymap_update K V M M1 :- custom_add K V M M1.
+  */
 
   typeabbrev (mymap K V) (std.map2 K V).
 
@@ -32,8 +36,143 @@ Elpi Accumulate lp:{{
   mymap_find K M V :- std.map2.find K M V.
   mymap_add K V M M1 :- std.map2.add K V M M1.
   mymap_update K V M M1 :- std.map2.update K V M M1.
+
+  /*
+  typeabbrev (mymap K V) (avl.tree K V).
+
+  mymap_make Cmp M :- avl.empty Cmp M.
+  mymap_find K M V :- avl.lookup_node K M V.
+  mymap_add K V M M1 :- avl.insert_node K V M M1.
+  mymap_update K V M M1 :- avl.update_node K V M M1.
+  */
 }}.
 
 Elpi Accumulate File TabledTC.
 
+(* Slow map test *)
+Elpi Accumulate lp:{{
+  pred map_test_fold i:int i:V i:V o:mymap int V.
+  map_test_fold I V U M :-
+    std.fold
+      {std.iota I}
+      {std.fold
+        {std.iota I}
+        {mymap_make cmp_term}
+        (k\m\ mymap_add k V m)
+      }
+      (k\m\ mymap_update k U m)
+      M
+    .
+}}.
+
+(* Elpi Accumulate lp:{{ *)
+(*   pred map_test_fold i:int, i:V, i:V, o:std.map int V. *)
+(*   map_test_fold I V U M :- *)
+(*    std.fold *)
+(*      {std.iota I} *)
+(*      {std.map.make cmp_term} *)
+(*      (k\m\ std.map.add k V m) /* insert */ *)
+(*      Mtemp, *)
+(*     std.fold *)
+(*       {std.iota I} *)
+(*       Mtemp *)
+(*       (k\m\ std.map.add k U m) /* update */ *)
+(*       M *)
+(*     . *)
+(* }}. *)
+
+(* Time Elpi Query lp:{{ *)
+(*   map_test_fold 6000 *)
+(*     {std.iota 20} *)
+(*     {std.iota 19} _. *)
+(* }}. *)
+
+Time Elpi Query lp:{{
+  map_test_fold 2000 {std.iota 20} {std.iota 19} _
+  .
+}}.
+
+(* Time Elpi Query lp:{{ *)
+(*   map_test_fold 6000 {std.iota 20} {std.iota 19} _ *)
+(*   . *)
+(* }}. *)
+
+(* Time Elpi Query lp:{{ *)
+(*   map_test_fold 7000 {std.iota 20} {std.iota 19} _ *)
+(*   . *)
+(* }}. *)
+
+(* Time Elpi Query lp:{{ *)
+(*   map_test_fold 8000 {std.iota 20} {std.iota 19} _ *)
+(*   . *)
+(* }}. *)
+
+(* Time Elpi Query lp:{{ *)
+(*   map_test_fold 9000 {std.iota 20} {std.iota 19} _ *)
+(*   . *)
+(* }}. *)
+
+(* Time Elpi Query lp:{{ *)
+(*   map_test_fold 10000 {std.iota 20} {std.iota 19} _ *)
+(*   . *)
+(* }}. *)
+
+(* Time Elpi Query lp:{{ *)
+(*   map_test_fold 11000 {std.iota 20} {std.iota 19} _ *)
+(*   . *)
+(* }}. *)
+
+(* Time Elpi Query lp:{{ *)
+(*   map_test_fold 12000 {std.iota 20} {std.iota 19} _ *)
+(*   . *)
+(* }}. *)
+
+(* Time Elpi Query lp:{{ *)
+(*   map_test_fold 13000 {std.iota 20} {std.iota 19} _ *)
+(*   . *)
+(* }}. *)
+
+(* Time Elpi Query lp:{{ *)
+(*   map_test_fold 14000 {std.iota 20} {std.iota 19} _ *)
+(*   . *)
+(* }}. *)
+
+(* Time Elpi Query lp:{{ *)
+(*   map_test_fold 28000 {std.iota 20} {std.iota 19} _ *)
+(*   . *)
+(* }}. *)
+
+Elpi Query lp:{{
+  coq.say "".
+}}.
+
 Elpi Export TC.TabledSolver.
+
+(* 6000: Finished transaction in 7.498 secs (7.454u,0.042s) (successful) *)
+(* 7000: Finished transaction in 11.687 secs (11.666u,0.003s) (successful) *)
+(* 8000: Finished transaction in 13.516 secs (13.502u,0.002s) (successful) *)
+(* 9000: Finished transaction in 16.214 secs (16.206u,0.002s) (successful) *)
+(* 10000: Finished transaction in 19.843 secs (19.817u,0.016s) (successful) *)
+(* 11000: Finished transaction in 24.429 secs (24.396u,0.002s) (successful) *)
+(* 12000: Finished transaction in 24.807 secs (24.791u,0.002s) (successful) *)
+(* 13000: Finished transaction in 32.366 secs (32.341u,0.008s) (successful) *)
+(* 14000: Finished transaction in 38.164 secs (38.044u,0.006s) (successful) *)
+
+(* Finished transaction in 37.175 secs (37.039u,0.128s) (successful) *)
+(* Finished transaction in 45.125 secs (45.017u,0.103s) (successful) *)
+(* Finished transaction in 58.736 secs (58.705u,0.026s) (successful) *)
+(* Finished transaction in 75.953 secs (75.914u,0.033s) (successful) *)
+(* Finished transaction in 97.66 secs (97.576u,0.075s) (successful) *)
+(* Finished transaction in 120.093 secs (120.063u,0.017s) (successful) *)
+(* Finished transaction in 147.584 secs (147.567u,0.001s) (successful) *)
+(* Finished transaction in 168.396 secs (168.328u,0.056s) (successful) *)
+(* Finished transaction in 197.791 secs (197.718u,0.057s) (successful) *)
+
+(* 3200 secs ~ 1 hour *)
+(* 866 secs ~ 14.5 min *)
+
+(* (6,37.175), (7,45.125), (8,58.736), (9,75.953), (10,97.66), (11,120.093), (12,147.584), (13,168.396), (14,197.791) *)
+(* 1.23069 x^2 - 4.04515 x + 14.5688 , R = 0.997845 *)
+
+
+(* 28000 *)
