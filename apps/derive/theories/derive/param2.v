b49b7b220951deb92fd5d5d11cdbd1a980cd3aa1
Tomas Vallejos <tomas.vallejos12@gmail.com>
2026-05-01 12:47:04 +0200

    derive.param2 moving db dispatch out of param2 program

--- apps/derive/theories/derive/param2.v
+++ apps/derive/theories/derive/param2.v
@@ -33,6 +33,31 @@ Elpi Db derive.param2.db lp:{{
 }}.
 #[superglobal] Elpi Accumulate derive.param2.db lp:{{
 
+
+  % helper to lift undeclared grefs to terms.
+  func global-gref gref, gref -> term.
+  global-gref (const _) GRR TR :- !,
+    coq.env.global GRR TR.
+  % GRR is the yet undeclared param translation of _GT.
+  global-gref _GT GRR (global GRR) :- !.
+
+  % queries param.gref and lifts answer to terms.
+  func dispatch-gref gref -> term,term.
+  dispatch-gref GRT U TR :-
+    param.gref GRT GRU GRR,
+    coq.env.global GRU U,
+    global-gref GRT GRR TR.
+
+  :name "param:gref"
+  param T U TR :- 
+    coq.env.global GRT T, !, 
+    dispatch-gref GRT U TR.
+
+  :name "paramR:gref"
+  paramR T U TR :- 
+    coq.env.global GRT T, !, 
+    dispatch-gref GRT U TR.
+
     :name "param:fail"
     param X _ _ :-
       M is "derive.param2: No binary parametricity translation for " ^
