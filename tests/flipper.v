a734c26c318849f248836454626d41d1e2492608
Davide Fissore <fisso9900@gmail.com>
2026-06-03 14:14:47 +0200

    clean test

--- tests/flipper.v
+++ tests/flipper.v
@@ -5,21 +5,28 @@ From elpi Require ext.tests.dummy.
 Elpi Command test.
 Elpi Accumulate Plugin "ext.elpi".
 Elpi Accumulate  lp:{{
-  pred encode term -> mysumT.
-  encode {{0}} (myC 0).
-  encode {{S lp:X}} (myC N1) :- 
-    encode X (myC N), N1 is N + 1.
+  func int2nat int -> term.
+  int2nat 0 {{0}} :- !.
+  int2nat N {{S lp:R}} :- N > 0, N' is N - 1, int2nat N' R.
+
+  func nat2int term -> int.
+  nat2int {{0}} 0.
+  nat2int {{S lp:R}} N :- nat2int R N', N is N' + 1.
+
+  func is_nat term ->.
+  is_nat {{0}}.
+  is_nat {{S lp:_}}.
+
+  func encode term -> mysumT.
+  encode T (myC N) :- is_nat T, !, nat2int T N.
   encode {{lp:X + lp:Y}} (myA X1 Y1) :- encode X X1, encode Y Y1.
 
-  pred decode mysumT -> term.
-  decode (myC 0) {{0}} :- !.
-  decode (myC N1) {{S lp:X}} :- !,
-    N2 is N1 - 1, decode (myC N2) X.
-  decode (myA X1 Y1) {{lp:X + lp:Y}} :- decode X1 X, decode Y1 Y.
-  decode A _ :- coq.error "A = " A.
+  func decode mysumT -> term.
+  decode (myC N) T :- int2nat N T.
+  decode (myA X Y) {{lp:X1 + lp:Y1}} :- decode X X1, decode Y Y1.
 }}.
 
 Elpi Query  lp:{{
   encode {{2 + 3}} X,
- compute X Z, decode Z R.
+  compute X Z, decode Z R.
 }}.
