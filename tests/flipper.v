cc0f71f7baa05165025ee4c21a541b6d64ef6bdc
thery <Laurent.Thery@inria.fr>
2026-06-03 13:05:17 +0200

    sum datatype

--- tests/flipper.v
+++ tests/flipper.v
@@ -5,12 +5,21 @@ From elpi Require ext.tests.dummy.
 Elpi Command test.
 Elpi Accumulate Plugin "ext.elpi".
 Elpi Accumulate  lp:{{
-  pred encode term -> mynat.
-  encode {{0}} myzero.
-  encode {{S lp:X}} (mysucc Y) :- encode X Y.
+  pred encode term -> mysumT.
+  encode {{0}} (myC 0).
+  encode {{S lp:X}} (myC N1) :- 
+    encode X (myC N), N1 is N + 1.
+  encode {{lp:X + lp:Y}} (myA X1 Y1) :- encode X X1, encode Y Y1.
+
+  pred decode mysumT -> term.
+  decode (myC 0) {{0}} :- !.
+  decode (myC N1) {{S lp:X}} :- !,
+    N2 is N1 - 1, decode (myC N2) X.
+  decode (myA X1 Y1) {{lp:X + lp:Y}} :- decode X1 X, decode Y1 Y.
+  decode A _ :- coq.error "A = " A.
 }}.
 
 Elpi Query  lp:{{
-  encode {{0}} X,
-  add1 X Z.
+  encode {{2 + 3}} X,
+ compute X Z, decode Z R.
 }}.
