8129b2046c91422f7990dbd3e2e17970bc348d2c
Davide Fissore <fisso9900@gmail.com>
2026-06-09 11:12:57 +0200

    more on projections

--- apps/tc/tests/test_proj.v
+++ apps/tc/tests/test_proj.v
@@ -1,12 +1,60 @@
 From elpi Require Import tc.
 
-Record r := mkr {car : Type; rf : car -> car}.
 
+Set TC NameShortPath.
+Class C (T : Type) := {f : T -> T}.
+Record r := mkr {car : Type; rf : car -> car}.
 Canonical Structure c := mkr nat (fun x => x).
 
-Class C (T : Type) := {f : T -> T}.
+Elpi Accumulate TC.Compiler lp:{{
+  % the goal is to check instances for C are correctly compiled
+  func is-class-C prop ->.
+  is-class-C (pi x\ X x) :- !, pi x\ is-class-C (X x).
+  is-class-C (tc-C _ _ :- _ as C) :- !, coq.say "Checking"C, if (expected-rule C) true (coq.error "Wrong compilation of" C).
+  is-class-C (tc-C _ _ as C) :- !, coq.say "Checking"C, if (expected-rule C) true (coq.error "Wrong compilation of" C).
+  is-class-C _.
 
-Instance inst c: C (car c). now constructor. Qed.
 
-Goal C nat. apply _. Qed.
+  func expected-rule -> prop.
+  pred dummy.
+  :name "x" dummy.
+  :before "tc-adder"
+  tc.add-tc-db _I _G C :- % coq.say "Compiled term is" C,
+    std.spy(is-class-C C), fail, !.
+}}.
 
+Module m1.
+  Elpi Accumulate TC.Compiler lp:{{
+    :after "x" expected-rule (tc-C {{nat}} _).
+  }}.
+
+  (* reducing the projection statically *)
+  Instance inst_red: C (car c). now constructor. Qed.
+
+  Elpi Accumulate TC.Compiler lp:{{
+    % removing the previous expected (best should be that the previous rule is local to the module)
+    :after "x" expected-rule _ :- !, fail.
+  }}.
+  (* Elpi Print TC.Compiler "elpi.apps.derive.tests/xxx".  *)
+
+  Goal C nat. apply _. Qed.
+  Goal C (car c). apply _. Qed.
+End m1.
+
+Module m2.
+  (* Mh, why this fails? *)
+  (* Elpi Accumulate TC.Compiler lp:{{
+    :after "x" expected-rule X :- !, coq.say "CIAO"X, std.spy(X = (tc-C Y _ :- [tc.link.proj _ _])), !.
+  }}. *)
+  Elpi Accumulate TC.Compiler lp:{{
+    % TODO: here I am doing to weak check, should make the previous Accumulate succeeds
+    :after "x" expected-rule X :- !, X = (tc-C Y _ :- [K_]), !.
+  }}.
+
+  (* cannot reduce the projection: c is quantified *)
+  Instance inst c: C (car c). now constructor. Qed.
+
+  (* need to use the chr *)
+  Goal C nat. apply _. Qed.
+  Goal C (car c). apply _. Qed.
+End m2.
