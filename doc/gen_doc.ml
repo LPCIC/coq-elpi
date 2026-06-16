d51b801f1f6dcf93ec35107f897ce319dfc95fb7
Davide Fissore <fisso9900@gmail.com>
2026-06-16 23:04:51 +0200

    wip

--- doc/gen_doc.ml
+++ doc/gen_doc.ml
@@ -1,3 +1,6 @@
-
+(* the builtins are taken from the src folder
+   this folder is a library called Ext_plug.
+   From ths Ext module we get the builtins constant
+*)
 let _ = Elpi.API.BuiltIn.document_file ~header:"% Generated"
   Ext_plug.Ext.builtins
