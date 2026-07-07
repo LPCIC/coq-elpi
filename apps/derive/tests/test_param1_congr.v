952c75c1424748bd5272c5439aa82bd33c1bbd3d
Jan-Oliver Kaiser <janno@skylabs-ai.com>
2026-07-07 14:50:08 +0200

    derive: support mutual inductives in param1_congr

--- apps/derive/tests/test_param1_congr.v
+++ apps/derive/tests/test_param1_congr.v
@@ -1,4 +1,4 @@
-From elpi.apps Require Import derive.param1_congr.
+From elpi.apps Require Import derive derive.param1_congr.
 
 From elpi.apps Require Import test_derive_corelib test_param1.
 Import test_derive_corelib.Coverage.
@@ -93,6 +93,69 @@ Redirect "tmp" Check congr_is_Build_pa_record : forall A PA n p1 p2, p1 = p2 -> 
 Redirect "tmp" Check congr_is_Build_pr_record : forall A pr n p1 p2, p1 = p2 -> forall b q1 q2, q1 = q2 -> is_Build_pr_record A pr n p1 b q1= is_Build_pr_record A pr n p2 b q2.
 Redirect "tmp" Check congr_is_E1 : is_E1 = is_E1.
 
+Module MutualStandaloneFirst.
+  From elpi.apps Require Import derive.param1_congr.
 
-Fail Redirect "tmp" Check congr_is_mpeano'.
-Fail Redirect "tmp" Check congr_is_mforest.
+  Inductive tree : Type := node (f : forest)
+  with forest : Type := empty | cons (t : tree) (f : forest).
+
+  Elpi derive.param1 tree.
+  Elpi derive.param1.congr is_tree.
+
+  Redirect "tmp" Check congr_is_node.
+  Redirect "tmp" Check congr_is_empty.
+  Redirect "tmp" Check congr_is_cons.
+End MutualStandaloneFirst.
+
+Module MutualStandaloneSecond.
+  From elpi.apps Require Import derive.param1_congr.
+
+  Inductive tree : Type := node (f : forest)
+  with forest : Type := empty | cons (t : tree) (f : forest).
+
+  Elpi derive.param1 tree.
+  Elpi derive.param1.congr is_forest.
+
+  Redirect "tmp" Check congr_is_node.
+  Redirect "tmp" Check congr_is_empty.
+  Redirect "tmp" Check congr_is_cons.
+End MutualStandaloneSecond.
+
+Module MutualMetaFirst.
+  From elpi.apps Require Import derive.param1_congr.
+
+  Inductive tree : Type := node (f : forest)
+  with forest : Type := empty | cons (t : tree) (f : forest).
+
+  #[only(param1,param1_congr)] derive tree.
+
+  Redirect "tmp" Check congr_is_node.
+  Redirect "tmp" Check congr_is_empty.
+  Redirect "tmp" Check congr_is_cons.
+End MutualMetaFirst.
+
+Module MutualMetaSecond.
+  From elpi.apps Require Import derive.param1_congr.
+
+  Inductive tree : Type := node (f : forest)
+  with forest : Type := empty | cons (t : tree) (f : forest).
+
+  #[only(param1,param1_congr)] derive forest.
+
+  Redirect "tmp" Check congr_is_node.
+  Redirect "tmp" Check congr_is_empty.
+  Redirect "tmp" Check congr_is_cons.
+End MutualMetaSecond.
+
+Module ParametrizedMutual.
+  From elpi.apps Require Import derive.param1_congr.
+
+  Inductive ptree (A : Type) : Type := pnode (x : A) (f : pforest)
+  with pforest (A : Type) : Type := pempty | pcons (t : ptree) (f : pforest).
+
+  #[only(param1,param1_congr)] derive ptree.
+
+  Redirect "tmp" Check congr_is_pnode.
+  Redirect "tmp" Check congr_is_pempty.
+  Redirect "tmp" Check congr_is_pcons.
+End ParametrizedMutual.
--- apps/derive/theories/derive/param1_congr.v
+++ apps/derive/theories/derive/param1_congr.v
@@ -8,6 +8,8 @@
    ------------------------------------------------------------------------- *)
 From elpi.apps.derive.elpi Extra Dependency "paramX_lib.elpi" as paramX.
 From elpi.apps.derive.elpi Extra Dependency "param1_congr.elpi" as param1_congr.
+From elpi.apps.derive.elpi Extra Dependency "mutual_lib.elpi" as mutual_lib.
+From elpi.apps.derive.elpi Extra Dependency "param1_mutual_lib.elpi" as param1_mutual_lib.
 From elpi.apps.derive.elpi Extra Dependency "derive_hook.elpi" as derive_hook.
 From elpi.apps.derive.elpi Extra Dependency "derive_synterp_hook.elpi" as derive_synterp_hook.
 
@@ -21,20 +23,33 @@ Elpi Db derive.param1.congr.db lp:{{
 
 Elpi Command derive.param1.congr.
 Elpi Accumulate File paramX.
+Elpi Accumulate File mutual_lib.
 Elpi Accumulate Db derive.param1.db.
+Elpi Accumulate File param1_mutual_lib.
 Elpi Accumulate Db derive.param1.congr.db.
 Elpi Accumulate File param1_congr.
 Elpi Accumulate lp:{{
+  func derive.param1.congr.standalone-main inductive, inductive, string -> list prop.
+  derive.param1.congr.standalone-main GR _IsGR Prefix C :-
+    derive.mutual.is-mutual GR, !,
+    derive.mutual.members GR TS,
+    std.map TS (t\c\ sigma IsT\
+      reali (global (indt t)) (global (indt IsT)),
+      derive.param1.congr.main (indt t) (indt IsT) Prefix c) CS,
+    std.flatten CS C.
+  derive.param1.congr.standalone-main GR IsGR Prefix C :-
+    derive.param1.congr.main (indt GR) (indt IsGR) Prefix C.
+
   main [str I, str O] :- !,
     coq.locate I (indt IsGR),
     realiR T {coq.env.global (indt IsGR)},
     coq.env.global (indt GR) T,
-    derive.param1.congr.main (indt GR) (indt IsGR) O _.
+    derive.param1.congr.standalone-main GR IsGR O _.
   main [str I] :- !,
     coq.locate I (indt IsGR),
     realiR T {coq.env.global (indt IsGR)},
     coq.env.global (indt GR) T,
-    derive.param1.congr.main (indt GR) (indt IsGR) "congr_" _.
+    derive.param1.congr.standalone-main GR IsGR "congr_" _.
   main _ :- usage.
 
   usage :-
@@ -44,6 +59,7 @@ Elpi Accumulate lp:{{
 
 (* hook into derive *)
 Elpi Accumulate derive Db derive.param1.congr.db.
+Elpi Accumulate derive File param1_mutual_lib.
 Elpi Accumulate derive File param1_congr.
 
 #[phases="both"] Elpi Accumulate derive lp:{{
@@ -56,6 +72,13 @@ dep1 "param1_congr" "param1".
 
 Elpi Accumulate derive lp:{{
 
-derivation T _ ff (derive "param1_congr" (derive.on_param1 T derive.param1.congr.main "congr_") (derive.on_param1 T (_\T\_\_\param1-congr-done T) _ _)).
+func derive.param1.congr.derive-main gref -> list prop.
+derive.param1.congr.derive-main (indt T) C :- derive.mutual-inductive T, !,
+  derive.mutual-inductives T TS,
+  std.map TS (t\c\ derive.on_param1 (indt t) derive.param1.congr.main "congr_" c) CS,
+  std.flatten CS C.
+derive.param1.congr.derive-main T C :- derive.on_param1 T derive.param1.congr.main "congr_" C.
+
+derivation T _ ff (derive "param1_congr" (derive.param1.congr.derive-main T) (derive.on_param1 T (_\T\_\_\param1-congr-done T) _ _)).
 
 }}.
