52179fa9f2db4a30ef233dae903f09e7f6f0bab2
Enrico Tassi <Enrico.Tassi@inria.fr>
2026-07-29 14:15:46 +0200

    Apply suggestion from @gares

--- apps/derive/tests/test_tag.v
+++ apps/derive/tests/test_tag.v
@@ -1,4 +1,4 @@
-From elpi.apps Require Import derive derive.tag.
+From elpi.apps Require Import derive.tag.
 
 From elpi.apps.derive.tests Require Import test_derive_corelib.
 Import test_derive_corelib.Coverage.
