00469ef3c67aabf8fa2bf92013a4388c55afe593
Enrico Tassi <Enrico.Tassi@inria.fr>
2026-08-05 11:19:39 +0200

    Apply suggestions from code review
    Co-authored-by: Enrico Tassi <Enrico.Tassi@inria.fr>

--- examples/example_plugin/doc/gen_doc.ml
+++ examples/example_plugin/doc/gen_doc.ml
@@ -1,6 +1,4 @@
-(* the builtins are taken from the src folder
-   this folder is a library called Ext_plug.
-   From ths Ext module we get the builtins constant
+(* the builtins are taken from the src folder, where the MyExtension_plugin library is defined
 *)
 let _ = Elpi.API.BuiltIn.document_file ~header:"% Generated"
   MyExtension_plugin.My_extension_ocaml_code.builtins
--- examples/example_plugin/src/my_extension_ocaml_code.ml
+++ examples/example_plugin/src/my_extension_ocaml_code.ml
@@ -1,13 +1,14 @@
 module API = Elpi.API
 module E = API.RawData
 
-(* An algrbraic data type in OCaml, with an API we want to expose *)
+(* An algerbraic data type with the API we want to expose: myType and compute *)
 type myType = Constant : int -> myType | SumOf : (myType * myType) -> myType
 let rec compute (s : myType) = match s with
   | Constant n -> n
   | SumOf (s1, s2) -> compute s1 + compute s2
 
-(* declaring the elpi symbol corresponding to the constructors of myType *)
+(* Declaring the elpi symbol corresponding to the constructors of myType
+   is optional, only needed if one users the RawData APIs *)
 let constant = E.Constants.declare_global_symbol "constant"
 let sumof = E.Constants.declare_global_symbol "sumof"
 
@@ -36,16 +37,10 @@ let compute_api =
       (fun a _ ~depth:_ -> (), Some (Constant (compute a)))),
     DocAbove)
 
-(* we declare the neame of the file in which the new API are declared
-   and what are the new elpi object defined *)
+(* we declare the neame of the file in which the new builtins are declared
+   and the exported stuff (data types and predicates) *)
 let builtins = API.BuiltIn.declare ~file_name:"myExtension.elpi" [
   MLData myType;
   compute_api
 ]
 
-(* The gen_doc tool will generate the Elpi code
-
-
-
-
-*)
