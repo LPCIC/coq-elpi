967ac652490e696dc04553a1c17834cb501b8416
VojtechStep <code@vojtechstep.eu>
2026-09-24 11:10:46 +0200

    Fix quoting of universe polymorphic primitive projections
    Universe polymorphic primitive projections would be quoted as global
    instead of pglobal.

--- src/rocq_elpi_glob_quotation.ml
+++ src/rocq_elpi_glob_quotation.ml
@@ -381,9 +381,6 @@ let gterm2lpast ~pattern ~language state glob =
   let coqloc = Option.default dummy_loc loc in
   let loc = Rocq_elpi_utils.of_coq_loc coqloc in
   match (DAst.get_thunk v) (*.CAst.v*) with
-  | GRef(GlobRef.ConstRef p,_ul) when Structures.PrimitiveProjections.mem p ->
-      let hd = in_elpiast_gr ~loc (GlobRef.ConstRef p) in
-      hd
   | GRef(gr, ul) when Global.is_polymorphic gr ->
     begin match ul with
     | None ->
